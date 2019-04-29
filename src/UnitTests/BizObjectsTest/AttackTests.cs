9569c3f32479e888f2b83aa1d70396af572e1974
Joshua Gage <josh@chezgage.com>
2019-04-28 22:02:16 -0700

    Add business objects unit tests

---
+++ src/UnitTests/BizObjectsTest/AttackTests.cs
@@ -0,0 +1,16 @@
+﻿using BizObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BizObjectsTest
+{
+    [TestClass]
+    class AttackTests
+    {
+        [TestMethod]
+        public void GetDate()
+        {
+            var attack = new Attack();
+        }
+
+    }
+}
