a412f6b20b3c66271f3ae18335b9ebe05c74b9ab
Joshua Gage <josh@chezgage.com>
2019-04-28 21:21:57 -0700

    Add test for RawLogLine and LogMessage

--- src/LineParser/LineDatum.cs
+++ src/LineParser/LineDatum.cs
@@ -10,13 +10,16 @@ namespace LineParser
         // This should be constant.
         // Q: What about log files in other languages/locales?
         private const int DateLength = 24;
+        private const int MessageStart = 27;
 
         public string RawLogLine { get; private set; }
         public DateTime LogTime { get; private set; }
+        public string LogMessage { get; private set; }
 
         public LineDatum(string logLine)
         {
             RawLogLine = logLine;
+            LogMessage = RawLogLine.Substring(MessageStart);
             LogTime = GetTime();
         }
 
@@ -26,9 +29,7 @@ namespace LineParser
             // [Sat Apr 20 19:23:28 2019]
 
             //int length = RawLogLine.IndexOf(']') - 1;
-            int length = DateLength;
-
-            var datePortion = RawLogLine.Substring(1, length);
+            var datePortion = RawLogLine.Substring(1, DateLength);
             return DateTime.ParseExact(datePortion, "ddd MMM dd HH:mm:ss yyyy", CultureInfo.InstalledUICulture);
         }
     }
--- src/UnitTests/LineParserTests/LineDatumTests.cs
+++ src/UnitTests/LineParserTests/LineDatumTests.cs
@@ -13,5 +13,19 @@ namespace LineParserTests
             var ld = new LineDatum(@"[Sat Apr 20 19:23:28 2019] It will take about 5 more seconds to prepare your camp.");
             Assert.AreEqual(new DateTime(2019, 4, 20, 19, 23, 28), ld.LogTime);
         }
+
+        [TestMethod]
+        public void GetRawLogLine()
+        {
+            var ld = new LineDatum(@"[Sat Apr 20 19:23:28 2019] It will take about 5 more seconds to prepare your camp.");
+            Assert.AreEqual(@"[Sat Apr 20 19:23:28 2019] It will take about 5 more seconds to prepare your camp.", ld.RawLogLine);
+        }
+
+        [TestMethod]
+        public void GetLogMessage()
+        {
+            var ld = new LineDatum(@"[Sat Apr 20 19:23:28 2019] It will take about 5 more seconds to prepare your camp.");
+            Assert.AreEqual(@"It will take about 5 more seconds to prepare your camp.", ld.LogMessage);
+        }
     }
 }
