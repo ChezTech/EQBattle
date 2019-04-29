11a65f4421d1553bed39a904dcdb860c0a64edc3
Joshua Gage <josh@chezgage.com>
2019-04-28 22:01:56 -0700

    Add project for business objects

---
+++ src/BizObjects/Attack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizObjects
+{
+    public class Attack
+    {
+        public string Attacker { get; private set; }
+        public string Defender { get; private set; }
+    }
+}
