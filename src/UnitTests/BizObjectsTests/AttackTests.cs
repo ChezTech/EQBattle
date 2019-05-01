5a9d2baf5a2a4d963d837a93b107ffb065007be9
Joshua Gage <josh@chezgage.com>
2019-05-01 08:50:52 -0700

    Rename test project/folder

---
+++ src/UnitTests/BizObjectsTests/AttackTests.cs
@@ -0,0 +1,24 @@
+﻿using BizObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BizObjectsTest
+{
+    [TestClass]
+    class AttackTests
+    {
+        [TestMethod]
+        public void VerifyAttacker()
+        {
+            var attack = new Attack();
+            Assert.AreEqual("aaaa", attack.Attacker);
+        }
+
+        [TestMethod]
+        public void VerifyDefender()
+        {
+            var attack = new Attack();
+            Assert.AreEqual("aaaa", attack.Defender);
+        }
+
+    }
+}
