56832b73bd6ccfd1bd145db7058fab4a1ccd2c8d
Joshua Gage <josh@chezgage.com>
2019-05-03 00:10:02 -0700

    Add additional properties for Hit class

--- src/BizObjects/Hit.cs
+++ src/BizObjects/Hit.cs
@@ -5,10 +5,19 @@ namespace BizObjects
 {
     public class Hit : Attack
     {
-        public Hit(LogDatum logLine, string attacker, string defender, Zone zone = null) : base(logLine, attacker, defender, zone)
+        public Hit(LogDatum logLine, string attacker, string defender, string attackVerb, int damage, string damageType, string damageBy, string damageQualifier, Zone zone = null) : base(logLine, attacker, defender, zone)
         {
+            AttackVerb = attackVerb;
+            Damage = damage;
+            DamageType = damageType;
+            DamageBy = damageBy;
+            DamageQualifier = damageQualifier;
         }
 
+        public string AttackVerb { get; private set; }
         public int Damage { get; private set; }
+        public string DamageType { get; private set; }
+        public string DamageBy { get; private set; }
+        public string DamageQualifier { get; private set; }
     }
 }
