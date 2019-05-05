865851ef8063f5ebe6bbab14b861fcb0b6919efa
Joshua Gage <josh@chezgage.com>
2019-05-04 20:31:46 -0700

    Handle new types of hits in our HitParser w/ unit tests

--- src/BizObjects/Parsers/HitParser.cs
+++ src/BizObjects/Parsers/HitParser.cs
@@ -23,6 +23,9 @@ namespace BizObjects.Parsers
 
 
         private readonly Regex RxYouHit; // https://regex101.com/r/tall6K/1
+        private readonly Regex RxOtherHitsYou; // https://regex101.com/r/OqdZme/2
+        private readonly Regex RxYourDamageShield; // https://regex101.com/r/7iMeAc/1
+        private readonly Regex RxDamageShieldOnYou; // https://regex101.com/r/jMfkL3/1
 
 
         //private readonly IList<string> VerbList = new List<string>() {
@@ -35,11 +38,20 @@ namespace BizObjects.Parsers
         public HitParser()
         {
             RxYouHit = new Regex(@"(You) \b(\w+)\b (.*) for (\d+) points? of( (.*))? damage( by (.*))?\.( \((.*)\))?", RegexOptions.Compiled);
+            RxOtherHitsYou = new Regex(@"(.*) \b(\w+)\b YOU for (\d+) points? of( (.*))? damage\.( \((.*)\))?", RegexOptions.Compiled);
+            RxYourDamageShield = new Regex(@"(.*) is (.*) by (.*) (.*) for (\d+) points of (.*) damage.", RegexOptions.Compiled);
+            RxDamageShieldOnYou = new Regex(@"(.*) are (.*) by (.*)'s (.*) for (\d+) points of (.*) damage!", RegexOptions.Compiled);
         }
 
         public bool TryParse(LogDatum logDatum, out ILine lineEntry)
         {
             if (TryParseYouHit(logDatum, out lineEntry))
+                return true;
+            if (TryParseOtherHitsYou(logDatum, out lineEntry))
+                return true;
+            if (TryParseYourDamageShield(logDatum, out lineEntry))
+                return true;
+            if (TryParseDamageShieldOnYou(logDatum, out lineEntry))
                 return true;
             return false;
         }
@@ -61,6 +73,75 @@ namespace BizObjects.Parsers
             var damageType = match.Groups[6].Success ? match.Groups[6].Value : null;
             var damageBy = match.Groups[8].Success ? match.Groups[8].Value : null;
             var damageQualifier = match.Groups[10].Success ? match.Groups[10].Value : null;
+
+            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
+
+            return true;
+        }
+
+        private bool TryParseOtherHitsYou(LogDatum logDatum, out ILine lineEntry)
+        {
+            var match = RxOtherHitsYou.Match(logDatum.LogMessage);
+
+            if (!match.Success)
+            {
+                lineEntry = null;
+                return false;
+            }
+
+            var attacker = match.Groups[1].Value;
+            var attackVerb = match.Groups[2].Value;
+            var defender = Attack.You;
+            var damage = int.Parse(match.Groups[3].Value);
+            string damageType = match.Groups[5].Success ? match.Groups[5].Value : null;
+            string damageBy = null;
+            var damageQualifier = match.Groups[7].Success ? match.Groups[7].Value : null;
+
+            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
+
+            return true;
+        }
+
+        private bool TryParseYourDamageShield(LogDatum logDatum, out ILine lineEntry)
+        {
+            var match = RxYourDamageShield.Match(logDatum.LogMessage);
+
+            if (!match.Success)
+            {
+                lineEntry = null;
+                return false;
+            }
+
+            var defender = match.Groups[1].Value;
+            var attackVerb = match.Groups[2].Value;
+            var attacker = Attack.You;
+            var damageBy = match.Groups[4].Value;
+            var damage = int.Parse(match.Groups[5].Value);
+            var damageType = match.Groups[6].Value;
+            string damageQualifier = null;
+
+            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
+
+            return true;
+        }
+
+        private bool TryParseDamageShieldOnYou(LogDatum logDatum, out ILine lineEntry)
+        {
+            var match = RxDamageShieldOnYou.Match(logDatum.LogMessage);
+
+            if (!match.Success)
+            {
+                lineEntry = null;
+                return false;
+            }
+
+            var defender = Attack.You;
+            var attackVerb = match.Groups[2].Value;
+            var attacker = match.Groups[3].Value;
+            var damageBy = match.Groups[4].Value;
+            var damage = int.Parse(match.Groups[5].Value);
+            var damageType = match.Groups[6].Value;
+            string damageQualifier = null;
 
             lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
 
--- src/UnitTests/BizObjectsTests/HitParserTests.cs
+++ src/UnitTests/BizObjectsTests/HitParserTests.cs
@@ -64,5 +64,62 @@ namespace BizObjectsTests
             Assert.AreEqual("Lynx Maw", hitEntry.DamageBy);
             Assert.AreEqual("Critical", hitEntry.DamageQualifier);
         }
+
+        [TestMethod]
+        public void YourDamageShield()
+        {
+            var logDatum = new LogDatum("[Fri Apr 26 09:26:36 2019] A cliknar adept is burned by YOUR flames for 896 points of non-melee damage.");
+
+            var result = new HitParser().TryParse(logDatum, out ILine lineEntry);
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(lineEntry is Hit);
+            var hitEntry = lineEntry as Hit;
+            Assert.AreEqual(Attack.You, hitEntry.Attacker);
+            Assert.AreEqual("a cliknar adept", hitEntry.Defender);
+            Assert.AreEqual(896, hitEntry.Damage);
+            Assert.AreEqual("burned", hitEntry.AttackVerb);
+            Assert.AreEqual("non-melee", hitEntry.DamageType);
+            Assert.AreEqual("flames", hitEntry.DamageBy);
+            Assert.IsNull(hitEntry.DamageQualifier);
+        }
+
+        [TestMethod]
+        public void DamageShieldOnYou()
+        {
+            var logDatum = new LogDatum("[Fri Apr 26 09:26:36 2019] YOU are pierced by a cliknar adept's thorns for 70 points of non-melee damage!");
+
+            var result = new HitParser().TryParse(logDatum, out ILine lineEntry);
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(lineEntry is Hit);
+            var hitEntry = lineEntry as Hit;
+            Assert.AreEqual(Attack.You, hitEntry.Defender);
+            Assert.AreEqual("a cliknar adept", hitEntry.Attacker);
+            Assert.AreEqual(70, hitEntry.Damage);
+            Assert.AreEqual("pierced", hitEntry.AttackVerb);
+            Assert.AreEqual("non-melee", hitEntry.DamageType);
+            Assert.AreEqual("thorns", hitEntry.DamageBy);
+            Assert.IsNull(hitEntry.DamageQualifier);
+        }
+
+        [TestMethod]
+        public void OtherAttacksYou()
+        {
+            var logDatum = new LogDatum("[Fri Apr 26 09:26:36 2019] A cliknar adept pierces YOU for 865 points of damage. (Strikethrough)");
+
+            var result = new HitParser().TryParse(logDatum, out ILine lineEntry);
+
+            Assert.IsTrue(result);
+            Assert.IsTrue(lineEntry is Hit);
+            var hitEntry = lineEntry as Hit;
+            Assert.AreEqual(Attack.You, hitEntry.Defender);
+            Assert.AreEqual("a cliknar adept", hitEntry.Attacker);
+            Assert.AreEqual(865, hitEntry.Damage);
+            Assert.AreEqual("pierces", hitEntry.AttackVerb);
+            Assert.IsNull(hitEntry.DamageType);
+            Assert.IsNull(hitEntry.DamageBy);
+            Assert.AreEqual("Strikethrough", hitEntry.DamageQualifier);
+        }
     }
 }
