fad80f9b9932f3c76bb7f1f0b491bb900f8bca78
Joshua Gage <josh@chezgage.com>
2019-05-07 15:03:55 -0700

    Remove the specific Regex's that we no longer need

--- src/BizObjects/Parsers/HitParser.cs
+++ src/BizObjects/Parsers/HitParser.cs
@@ -13,25 +13,11 @@ namespace BizObjects.Parsers
         private readonly string regexHit = @"(.+) (**verbs**) (.+) for (\d+) points? of(?: (.+))? damage(?: by (.+))?\.(?: \((.+)\))?"; // https://regex101.com/r/bc2GRX/2
         private readonly string regexDamageShield = @"(.+) (?:is|are) (**verbs**) by (.+) (.+) for (\d+) points? of(?: (.+))? damage(?: by (.+))?[.!](?: \((.+)\))?"; // https://regex101.com/r/uerSMk/2/
 
-        private readonly Regex RxYouHit; // https://regex101.com/r/tall6K/1
-        private readonly Regex RxOtherHitsYou; // https://regex101.com/r/OqdZme/2
-        private readonly Regex RxYourDamageShield; // https://regex101.com/r/7iMeAc/1
-        private readonly Regex RxDamageShieldOnYou; // https://regex101.com/r/jMfkL3/1
-        private readonly Regex RxPet; // https://regex101.com/r/DHZqDT/1
-
         public HitParser()
         {
             string verbs = string.Join('|', new AttackTypeConverter().Names);
             RxHit = new Regex(regexHit.Replace("**verbs**", verbs), RegexOptions.Compiled);
             RxDamageShield = new Regex(regexDamageShield.Replace("**verbs**", verbs), RegexOptions.Compiled);
-
-            RxYouHit = new Regex(@"(You) \b(\w+)\b (.*) for (\d+) points? of( (.*))? damage( by (.*))?\.( \((.*)\))?", RegexOptions.Compiled);
-            RxOtherHitsYou = new Regex(@"(.*) \b(\w+)\b YOU for (\d+) points? of( (.*))? damage\.( \((.*)\))?", RegexOptions.Compiled);
-            RxYourDamageShield = new Regex(@"(.*) is (.*) by (.*) (.*) for (\d+) points of (.*) damage.", RegexOptions.Compiled);
-            RxDamageShieldOnYou = new Regex(@"(.*) are (.*) by (.*)'s (.*) for (\d+) points of (.*) damage!", RegexOptions.Compiled);
-
-            // Note: possesive is written using a back-tick, *not* an apostrophe (as should be)
-            RxPet = new Regex(@"(.*)`s pet \b(\w+)\b (.*) for (\d+) points? of( (.*))? damage( by (.*))?\.( \((.*)\))?", RegexOptions.Compiled);
         }
 
         public bool TryParse(LogDatum logDatum, out ILine lineEntry)
@@ -43,16 +29,6 @@ namespace BizObjects.Parsers
             if (TryParseHit(logDatum, out lineEntry))
                 return true;
 
-            //if (TryParseYouHit(logDatum, out lineEntry))
-            //    return true;
-            //if (TryParseOtherHitsYou(logDatum, out lineEntry))
-            //    return true;
-            //if (TryParseYourDamageShield(logDatum, out lineEntry))
-            //    return true;
-            //if (TryParseDamageShieldOnYou(logDatum, out lineEntry))
-            //    return true;
-            //if (TryParsePet(logDatum, out lineEntry))
-            //    return true;
             return false;
         }
         private bool TryParseHit(LogDatum logDatum, out ILine lineEntry)
@@ -100,122 +76,5 @@ namespace BizObjects.Parsers
 
             return true;
         }
-
-        private bool TryParseYouHit(LogDatum logDatum, out ILine lineEntry)
-        {
-            var match = RxYouHit.Match(logDatum.LogMessage);
-
-            if (!match.Success)
-            {
-                lineEntry = null;
-                return false;
-            }
-
-            var attacker = Attack.You;
-            var attackVerb = match.Groups[2].Value;
-            var defender = match.Groups[3].Value;
-            var damage = int.Parse(match.Groups[4].Value);
-            var damageType = match.Groups[6].Success ? match.Groups[6].Value : null;
-            var damageBy = match.Groups[8].Success ? match.Groups[8].Value : null;
-            var damageQualifier = match.Groups[10].Success ? match.Groups[10].Value : null;
-
-            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
-
-            return true;
-        }
-
-        private bool TryParseOtherHitsYou(LogDatum logDatum, out ILine lineEntry)
-        {
-            var match = RxOtherHitsYou.Match(logDatum.LogMessage);
-
-            if (!match.Success)
-            {
-                lineEntry = null;
-                return false;
-            }
-
-            var attacker = match.Groups[1].Value;
-            var attackVerb = match.Groups[2].Value;
-            var defender = Attack.You;
-            var damage = int.Parse(match.Groups[3].Value);
-            string damageType = match.Groups[5].Success ? match.Groups[5].Value : null;
-            string damageBy = null;
-            var damageQualifier = match.Groups[7].Success ? match.Groups[7].Value : null;
-
-            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
-
-            return true;
-        }
-
-        private bool TryParseYourDamageShield(LogDatum logDatum, out ILine lineEntry)
-        {
-            var match = RxYourDamageShield.Match(logDatum.LogMessage);
-
-            if (!match.Success)
-            {
-                lineEntry = null;
-                return false;
-            }
-
-            var defender = match.Groups[1].Value;
-            var attackVerb = match.Groups[2].Value;
-            var attacker = Attack.You;
-            var damageBy = match.Groups[4].Value;
-            var damage = int.Parse(match.Groups[5].Value);
-            var damageType = match.Groups[6].Value;
-            string damageQualifier = null;
-
-            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
-
-            return true;
-        }
-
-        private bool TryParseDamageShieldOnYou(LogDatum logDatum, out ILine lineEntry)
-        {
-            var match = RxDamageShieldOnYou.Match(logDatum.LogMessage);
-
-            if (!match.Success)
-            {
-                lineEntry = null;
-                return false;
-            }
-
-            var defender = Attack.You;
-            var attackVerb = match.Groups[2].Value;
-            var attacker = match.Groups[3].Value;
-            var damageBy = match.Groups[4].Value;
-            var damage = int.Parse(match.Groups[5].Value);
-            var damageType = match.Groups[6].Value;
-            string damageQualifier = null;
-
-            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier);
-
-            return true;
-        }
-
-        private bool TryParsePet(LogDatum logDatum, out ILine lineEntry)
-        {
-            var match = RxPet.Match(logDatum.LogMessage);
-
-            if (!match.Success)
-            {
-                lineEntry = null;
-                return false;
-            }
-
-            var attacker = match.Groups[1].Value;
-            var isPet = true;
-            var attackVerb = match.Groups[2].Value;
-            var defender = match.Groups[3].Value;
-            var damage = int.Parse(match.Groups[4].Value);
-            var damageType = match.Groups[6].Success ? match.Groups[6].Value : null;
-            var damageBy = match.Groups[8].Success ? match.Groups[8].Value : null;
-            var damageQualifier = match.Groups[10].Success ? match.Groups[10].Value : null;
-
-            lineEntry = new Hit(logDatum, attacker, defender, attackVerb, damage, damageType, damageBy, damageQualifier, isPet);
-
-            return true;
-        }
-
     }
 }
