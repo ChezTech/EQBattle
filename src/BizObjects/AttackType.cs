762e28557642802b4a6b675361161958f7082e10
Joshua Gage <josh@chezgage.com>
2019-05-06 23:29:06 -0700

    Add attack type enum and converter to help with regex parsing using the attack verbs

---
+++ src/BizObjects/AttackType.cs
@@ -0,0 +1,24 @@
+﻿namespace BizObjects
+{
+    public enum AttackType
+    {
+        Unknown,
+        Backstab,
+        Bash,
+        Bite,
+        Claw,
+        Crush,
+        Frenzy,
+        Gore,
+        Hit,
+        Kick,
+        Maul,
+        Pierce,
+        Punch,
+        Slash,
+        Slice,
+        Smash,
+        Sting,
+        Strike,
+    }
+}
---
+++ src/BizObjects/AttackTypeConverter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace BizObjects
+{
+    public class AttackTypeConverter
+    {
+        private readonly IDictionary<string, AttackType> _nameToTypeMap = new Dictionary<string, AttackType>()
+        {
+            {"backstab", AttackType.Backstab },
+            {"backstabs", AttackType.Backstab },
+            {"bash", AttackType.Bash },
+            {"bashes", AttackType.Bash },
+            {"bite", AttackType.Bite },
+            {"bites", AttackType.Bite },
+            {"claw", AttackType.Claw },
+            {"claws", AttackType.Claw },
+            {"crush", AttackType.Crush },
+            {"crushes", AttackType.Crush },
+            {"frenzies on", AttackType.Frenzy },
+            {"gore", AttackType.Gore },
+            {"gores", AttackType.Gore },
+            {"hit", AttackType.Hit },
+            {"hits", AttackType.Hit },
+            {"kick", AttackType.Kick },
+            {"kicks", AttackType.Kick },
+            {"maul", AttackType.Maul },
+            {"mauls", AttackType.Maul },
+            {"pierce", AttackType.Pierce },
+            {"pierced", AttackType.Pierce },
+            {"pierces", AttackType.Pierce },
+            {"punch", AttackType.Punch },
+            {"punches", AttackType.Punch },
+            {"slash", AttackType.Slash },
+            {"slashes", AttackType.Slash },
+            {"slice", AttackType.Slice },
+            {"slices", AttackType.Slice },
+            {"smash", AttackType.Smash },
+            {"smashes", AttackType.Smash },
+            {"sting", AttackType.Sting },
+            {"stings", AttackType.Sting },
+            {"strike", AttackType.Strike },
+            {"strikes", AttackType.Strike },
+            //{"xxxx", AttackType.Unknown },
+        };
+
+        public AttackType Convert(string hitTypeName)
+        {
+            return _nameToTypeMap.TryGetValue(hitTypeName, out AttackType hitType) ? hitType : AttackType.Unknown;
+        }
+
+        public ICollection<string> Names { get { return _nameToTypeMap.Keys; } }
+    }
+}
