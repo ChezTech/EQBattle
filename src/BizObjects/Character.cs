2369a53526cb0020e2ce894c00e6b4216cc679e8
Joshua Gage <josh@chezgage.com>
2019-05-08 11:11:26 -0700

    Unit test to verify conversion of "A", "An" to lowercase

--- src/BizObjects/Character.cs
+++ src/BizObjects/Character.cs
@@ -5,13 +5,28 @@ namespace BizObjects
 {
     public class Character
     {
+        public const string You = "You";
+
         public string Name { get; }
         public bool IsPet { get; }
 
         public Character(string name)
         {
-            Name = name;
+            Name = CleanName(name);
             IsPet = false;
+        }
+
+        private string CleanName(string name)
+        {
+            return name
+                .Replace("YOUR", You)
+                .Replace("YOU", You)
+                .Replace("You", You) // This sets us up to changing what the 'You' property actually points to (once we can figure out the character's actual name)
+                .Replace("A ", "a ") // Will this get only the "A monster type" at the beginning? Could use RegEx.Replace ....
+                .Replace("An ", "an ")
+                // // .Replace("`s", "") // backtick: better regex? (E.g. "Bob`s pet kicked ....", "Joe kicked Bob`s pet ....")
+                // .Replace("'s", "") // apostrophe: Can we replace this by a better Regex? (E.g. "... pierced by a monster's thorns...")
+                ;
         }
     }
 }
--- src/UnitTests/BizObjectsTests/CharacterTests.cs
+++ src/UnitTests/BizObjectsTests/CharacterTests.cs
@@ -12,5 +12,40 @@ namespace BizObjectsTest
             var c = new Character("Khadaji");
             Assert.AreEqual("Khadaji", c.Name);
         }
+
+        [TestMethod]
+        public void ConvertUppercaseYou()
+        {
+            var c = new Character("YOU");
+            Assert.AreEqual("You", c.Name);
+        }
+
+        [TestMethod]
+        public void ConvertYour()
+        {
+            var c = new Character("YOUR");
+            Assert.AreEqual("You", c.Name);
+        }
+
+        [TestMethod]
+        public void ConvertCapitalACapitalMonster()
+        {
+            var c = new Character("A Razorfiend Subduer");
+            Assert.AreEqual("a Razorfiend Subduer", c.Name);
+        }
+
+        [TestMethod]
+        public void ConvertCapitalALowerMonster()
+        {
+            var c = new Character("A cliknar hunter");
+            Assert.AreEqual("a cliknar hunter", c.Name);
+        }
+
+        [TestMethod]
+        public void ConvertCapitalAnMonster()
+        {
+            var c = new Character("An awakened citizen");
+            Assert.AreEqual("an awakened citizen", c.Name);
+        }
     }
 }
