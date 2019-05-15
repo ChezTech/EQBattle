894e093fdc45950f32879bcdc06dff18720c53a9
Joshua Gage <josh@chezgage.com>
2019-05-15 15:22:57 -0700

    Switch to FighterStatistics for Battle also

--- src/BizObjects/Battle.cs
+++ src/BizObjects/Battle.cs
@@ -13,43 +13,26 @@ namespace BizObjects
         public string Zone { get; }
         private ConcurrentDictionary<Character, Fighter> _fighters = new ConcurrentDictionary<Character, Fighter>();
 
-        /// All damage dealt, from both PCs and NPCs (mercs + mobs)
-        public int TotalDamageDealt { get => Fighters.Sum(x => x.TotalDamageDealt); }
-        public int TotalDamageTaken { get => Fighters.Sum(x => x.TotalDamageTaken); }
+        // /// All damage dealt, from both PCs and NPCs (mercs + mobs)
+        // public int TotalDamageDealt { get => Fighters.Sum(x => x.TotalDamageDealt); }
+        // public int TotalDamageTaken { get => Fighters.Sum(x => x.TotalDamageTaken); }
 
         public void AddLine(Attack line)
         {
             var attackChar = _fighters.GetOrAdd(line.Attacker, new Fighter(line.Attacker));
-            attackChar.AddLine(line);
             attackChar.AddOffense(line);
 
             var defendChar = _fighters.GetOrAdd(line.Defender, new Fighter(line.Defender));
-            defendChar.AddLine(line);
             defendChar.AddDefense(line);
         }
-
-        // public void AddLine(Hit line)
-        // {
-        //     var attackChar = _fighters.GetOrAdd(line.Attacker, new Fighter(line.Attacker));
-        //     attackChar.AddOffense(line);
-
-        //     var defendChar = _fighters.GetOrAdd(line.Defender, new Fighter(line.Defender));
-        //     defendChar.AddDefense(line);
-        // }
-        // public void AddLine(Miss line)
-        // {
-        //     var attackChar = _fighters.GetOrAdd(line.Attacker, new Fighter(line.Attacker));
-        //     attackChar.AddOffense(line);
-
-        //     var defendChar = _fighters.GetOrAdd(line.Defender, new Fighter(line.Defender));
-        //     defendChar.AddDefense(line);
-        // }
 
         public void AddLine(Heal line)
         {
             var healerChar = _fighters.GetOrAdd(line.Healer, new Fighter(line.Healer));
+            healerChar.AddOffense(line);
 
             var patientChar = _fighters.GetOrAdd(line.Patient, new Fighter(line.Patient));
+            patientChar.AddDefense(line);
         }
 
         // public void AddLine(Kill line)
--- src/UnitTests/BizObjectsTests/BattleTests.cs
+++ src/UnitTests/BizObjectsTests/BattleTests.cs
@@ -56,19 +56,15 @@ namespace BizObjectsTests
             Assert.IsTrue(chars.Any(x => x.Name == "Bealica"));
             Assert.IsTrue(chars.Any(x => x.Name == "a dwarf disciple"));
 
-            Assert.AreEqual(4507, battle.Fighters.First(x => x.Character.Name == "Khadaji").TotalDamageDealt);
             Assert.AreEqual(4507, battle.Fighters.First(x => x.Character.Name == "Khadaji").OffensiveStatistics.TotalDamage);
+            Assert.AreEqual(11481, battle.Fighters.First(x => x.Character.Name == "Bealica").OffensiveStatistics.TotalDamage);
+            Assert.AreEqual(4106, battle.Fighters.First(x => x.Character.Name == "a dwarf disciple").OffensiveStatistics.TotalDamage);
+            // Assert.AreEqual(20094, battle.TotalDamageDealt);
 
-            Assert.AreEqual(11481, battle.Fighters.First(x => x.Character.Name == "Bealica").TotalDamageDealt);
-            Assert.AreEqual(4106, battle.Fighters.First(x => x.Character.Name == "a dwarf disciple").TotalDamageDealt);
-            Assert.AreEqual(20094, battle.TotalDamageDealt);
-
-            Assert.AreEqual(4106, battle.Fighters.First(x => x.Character.Name == "Khadaji").TotalDamageTaken);
             Assert.AreEqual(4106, battle.Fighters.First(x => x.Character.Name == "Khadaji").DefensiveStatistics.TotalDamage);
-
-            Assert.AreEqual(0, battle.Fighters.First(x => x.Character.Name == "Bealica").TotalDamageTaken);
-            Assert.AreEqual(15988, battle.Fighters.First(x => x.Character.Name == "a dwarf disciple").TotalDamageTaken);
-            Assert.AreEqual(20094, battle.TotalDamageTaken);
+            Assert.AreEqual(0, battle.Fighters.First(x => x.Character.Name == "Bealica").DefensiveStatistics.TotalDamage);
+            Assert.AreEqual(15988, battle.Fighters.First(x => x.Character.Name == "a dwarf disciple").DefensiveStatistics.TotalDamage);
+            // Assert.AreEqual(20094, battle.TotalDamageTaken);
         }
 
     }
