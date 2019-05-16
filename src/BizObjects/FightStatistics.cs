87667469538ad368985348b5a2ab215b65174b6e
Joshua Gage <josh@chezgage.com>
2019-05-16 13:08:34 -0700

    Add unit tests for FightStatistics class

--- src/BizObjects/FightStatistics.cs
+++ src/BizObjects/FightStatistics.cs
@@ -10,10 +10,10 @@ namespace BizObjects
         public int TotalDamage { get => Lines.Where(x => x is Hit).Select(x => x as Hit).Sum(x => x.Damage); }
         public int MaxHitDamage { get => Lines.Where(x => x is Hit).Select(x => x as Hit).Max(x => x.Damage); }
         public int MinHitDamage { get => Lines.Where(x => x is Hit).Select(x => x as Hit).Min(x => x.Damage); }
-        public int HitCount { get; }
-        public int MissCount { get; }
+        public int HitCount { get => Lines.Where(x => x is Hit).Count(); }
+        public int MissCount { get => Lines.Where(x => x is Miss).Count(); }
         public int KillCount { get => Lines.Where(x => x is Kill).Select(x => x as Kill).Count(); }
-        public double HitPercentage { get; }
+        public double HitPercentage { get => (double)HitCount / (HitCount + MissCount); }
         public int HealAmount { get => Lines.Where(x => x is Heal).Select(x => x as Heal).Sum(x => x.Amount); }
         public IDictionary<string, int> DamagePerType { get; }
         public IDictionary<string, int> HitsPerType { get; }
---
+++ src/UnitTests/BizObjectsTests/FightStatisticsTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using BizObjects;
+using LineParser;
+using LineParser.Parsers;
+using LogObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BizObjectsTests
+{
+    [TestClass]
+    public class FightStatisticsTests
+    {
+        private static readonly YouResolver YouAre = new YouResolver("Khadaji");
+        private LineParserFactory _parser = new LineParserFactory();
+        private readonly IParser _hitParser = new HitParser(YouAre);
+        private readonly IParser _missParser = new MissParser(YouAre);
+        private readonly IParser _healParser = new HealParser(YouAre);
+        private readonly IParser _killParser = new KillParser(YouAre);
+
+
+        public FightStatisticsTests()
+        {
+            _parser.AddParser(_hitParser, null);
+            _parser.AddParser(_missParser, null);
+            _parser.AddParser(_healParser, null);
+            _parser.AddParser(_killParser, null);
+        }
+
+        [TestMethod]
+        public void SmallFightBasicStats()
+        {
+            var fightStats = new FightStatistics();
+
+            // Offensive LogLines only
+            fightStats.AddLine((dynamic)_parser.ParseLine(new LogDatum("[Fri Apr 05 16:16:42 2019] Khadaji hit a dwarf disciple for 2 points of magic damage by Distant Strike I.")));
+            fightStats.AddLine((dynamic)_parser.ParseLine(new LogDatum("[Fri Apr 05 16:16:45 2019] A dwarf disciple is pierced by YOUR thorns for 60 points of non-melee damage.")));
+            fightStats.AddLine((dynamic)_parser.ParseLine(new LogDatum("[Fri Apr 05 16:16:49 2019] You kick a dwarf disciple for 3041 points of damage. (Strikethrough)")));
+            fightStats.AddLine((dynamic)_parser.ParseLine(new LogDatum("[Fri Apr 05 16:16:50 2019] You try to crush a dwarf disciple, but miss!")));
+            fightStats.AddLine((dynamic)_parser.ParseLine(new LogDatum("[Fri Apr 05 16:16:51 2019] Khadaji hit a dwarf disciple for 892 points of poison damage by Strike of Venom IV. (Critical)")));
+            fightStats.AddLine((dynamic)_parser.ParseLine(new LogDatum("[Fri Apr 05 16:17:20 2019] Khadaji hit a dwarf disciple for 512 points of chromatic damage by Lynx Maw.")));
+
+            Assert.AreEqual(4507, fightStats.TotalDamage);
+            Assert.AreEqual(3041, fightStats.MaxHitDamage);
+            Assert.AreEqual(2, fightStats.MinHitDamage);
+            Assert.AreEqual(5, fightStats.HitCount);
+            Assert.AreEqual(1, fightStats.MissCount);
+            Assert.AreEqual(0, fightStats.KillCount);
+            Assert.AreEqual(0.833, fightStats.HitPercentage, 0.001);
+        }
+    }
+}
