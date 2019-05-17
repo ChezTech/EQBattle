e708bf1b1ee044f756762b0abfe45b1ae20976dd
Joshua Gage <josh@chezgage.com>
2019-05-17 16:27:32 -0700

    Move a few properties from Line into ILine

--- src/BizObjects/ILine.cs
+++ src/BizObjects/ILine.cs
@@ -1,6 +1,11 @@
-﻿namespace BizObjects
+﻿using System;
+using LogObjects;
+
+namespace BizObjects
 {
     public interface ILine
     {
+        DateTime Time { get; }
+        LogDatum LogLine { get; }
     }
 }
