3884ba23fa259301b5186eeae99f3828987c87f5
Joshua Gage <josh@chezgage.com>
2019-05-19 21:22:24 -0700

    Rename War to Battle

--- src/BizObjects/Battle.cs
+++ src/BizObjects/Battle.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace BizObjects
 {
-    public class War
+    public class Battle
     {
         public IList<Fight> Fights { get; private set; }
         public IList<Character> Fighters { get; private set; }
