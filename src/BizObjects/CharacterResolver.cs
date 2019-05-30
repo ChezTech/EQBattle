47be8e05e9b9559152c0a910da535e712d454f76
Joshua Gage <josh@chezgage.com>
2019-05-30 13:53:30 -0700

    Add support and test for characters types other than PC

--- src/BizObjects/CharacterResolver.cs
+++ src/BizObjects/CharacterResolver.cs
@@ -21,9 +21,9 @@ namespace BizObjects
         }
 
         public ISet<string> PCNames { get; } = new HashSet<string>();
-        public IEnumerable<string> NPCNames { get; } = new HashSet<string>();
-        public IEnumerable<string> PetNames { get; } = new HashSet<string>();
-        public IEnumerable<string> MercenaryNames { get; } = new HashSet<string>();
+        public ISet<string> NPCNames { get; } = new HashSet<string>();
+        public ISet<string> PetNames { get; } = new HashSet<string>();
+        public ISet<string> MercenaryNames { get; } = new HashSet<string>();
 
         public Type WhichType(Character c) => WhichType(c.Name);
         public Type WhichType(string name)
@@ -37,5 +37,11 @@ namespace BizObjects
 
         public void AddPlayer(Character c) => AddPlayer(c.Name);
         public void AddPlayer(string name) => PCNames.Add(name);
+        public void AddNonPlayer(Character c) => AddNonPlayer(c.Name);
+        public void AddNonPlayer(string name) => NPCNames.Add(name);
+        public void AddPet(Character c) => AddPet(c.Name);
+        public void AddPet(string name) => PetNames.Add(name);
+        public void AddMercenary(Character c) => AddMercenary(c.Name);
+        public void AddMercenary(string name) => MercenaryNames.Add(name);
     }
 }
--- src/UnitTests/BizObjectsTests/CharacterResolverTests.cs
+++ src/UnitTests/BizObjectsTests/CharacterResolverTests.cs
@@ -19,5 +19,44 @@ namespace BizObjectsTests
             var cr = new CharacterResolver();
             Assert.AreEqual(CharacterResolver.Type.Unknown, cr.WhichType(new Character("Khadaji")));
         }
+
+        [TestMethod]
+        public void AddPCGetPC()
+        {
+            var k = new Character("Khadaji");
+            var cr = new CharacterResolver();
+            cr.AddPlayer(k);
+            Assert.AreEqual(CharacterResolver.Type.Player, cr.WhichType(k));
+            Assert.AreEqual(CharacterResolver.Type.Player, cr.WhichType("Khadaji"));
+        }
+
+        [TestMethod]
+        public void AddVarietyOfNamesGetThemBackCorrectly()
+        {
+            var cr = new CharacterResolver();
+            cr.AddPlayer("Khadaji");
+            cr.AddMercenary("Movanna");
+            cr.AddPet("Khadaji`s pet");
+            cr.AddPet("Jabantik");
+            cr.AddNonPlayer("Gomphus");
+
+            Assert.AreEqual(CharacterResolver.Type.Player, cr.WhichType("Khadaji"));
+            Assert.AreEqual(CharacterResolver.Type.Mercenary, cr.WhichType("Movanna"));
+            Assert.AreEqual(CharacterResolver.Type.Pet, cr.WhichType("Khadaji`s pet"));
+            Assert.AreEqual(CharacterResolver.Type.Pet, cr.WhichType("Jabantik"));
+            Assert.AreEqual(CharacterResolver.Type.NonPlayerCharacter, cr.WhichType("Gomphus"));
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void AddNameToMultipleListsGetBackWhichShouldNotBeAllowed()
+        {
+            var cr = new CharacterResolver();
+            cr.AddMercenary("Bob");
+            cr.AddNonPlayer("Bob");
+            // Should this fail? Exception or silent? I prefer no exception
+
+            Assert.AreEqual(CharacterResolver.Type.Mercenary, cr.WhichType("Bob"));
+        }
     }
 }
