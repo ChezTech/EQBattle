e8a233184b3b307830f58a225b499502f18722b6
Joshua Gage <josh@chezgage.com>
2019-05-31 11:22:46 -0700

    Add some more zone line tests

--- src/UnitTests/LineParserTests/ZoneParserTests.cs
+++ src/UnitTests/LineParserTests/ZoneParserTests.cs
@@ -13,6 +13,10 @@ namespace LineParserTests
         [DataTestMethod]
         [DataRow("[Sat Mar 30 10:42:39 2019] You have entered Arthicrex.", "Arthicrex")]
         [DataRow("[Sat Mar 30 10:42:39 2019] You have entered Some Complex Zone Name.", "Some Complex Zone Name")]
+        [DataRow("[Sat Mar 30 07:09:27 2019] You have entered Toxxulia Forest.", "Toxxulia Forest")]
+        [DataRow("[Sat Mar 30 07:11:03 2019] You have entered Paineel.", "Paineel")]
+        [DataRow("[Sat Mar 30 07:12:03 2019] You have entered The Ruins of Old Paineel.", "The Ruins of Old Paineel")]
+
         // [DataRow("xxxxxx", "zzzzz")]
         public void ZoneTets(string logLine, string zoneName)
         {
