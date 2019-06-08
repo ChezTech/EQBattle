52a57109e15aa70b15c6995c76802dc91e794190
Joshua Gage <josh@chezgage.com>
2019-06-07 18:28:36 -0700

    Extract the line setup to common method

--- src/UnitTests/BizObjectsTests/CharacterTrackerTests.cs
+++ src/UnitTests/BizObjectsTests/CharacterTrackerTests.cs
@@ -1,4 +1,5 @@
-﻿using BizObjects;
+﻿using System;
+using BizObjects;
 using LineParser;
 using LineParser.Parsers;
 using LogObjects;
@@ -10,8 +11,7 @@ namespace BizObjectsTests
     public class CharacterTrackerTests
     {
         private static readonly YouResolver YouAre = new YouResolver("Khadaji");
-        // private static readonly CharacterResolver CharResolver = new CharacterResolver();
-        private LineParserFactory _parser = new LineParserFactory();
+        private static LineParserFactory _parser = new LineParserFactory();
         private readonly IParser _hitParser = new HitParser(YouAre);
         private readonly IParser _missParser = new MissParser(YouAre);
         private readonly IParser _healParser = new HealParser(YouAre);
@@ -27,12 +27,18 @@ namespace BizObjectsTests
             _parser.AddParser(_whoParser, null);
         }
 
+        private readonly Action<CharacterTracker, string> TrackLine = (tracker, logLine) =>
+        {
+            ILine line = _parser.ParseLine(new LogDatum(logLine));
+            tracker.TrackLine((dynamic)line);
+        };
+
         [TestMethod]
         public void IdentifyCharYoureAttackingAsAMob()
         {
             var tracker = SetupNewTracker(out CharacterResolver charResolver);
 
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Mon May 27 09:56:06 2019] Khadaji kicks Gomphus for 871 points of damage. (Critical)")));
+            TrackLine(tracker, "[Mon May 27 09:56:06 2019] Khadaji kicks Gomphus for 871 points of damage. (Critical)");
 
             Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khadaji"));
             Assert.AreEqual(CharacterResolver.Type.NonPlayerCharacter, charResolver.WhichType("Gomphus"));
@@ -43,7 +49,7 @@ namespace BizObjectsTests
         {
             var tracker = SetupNewTracker(out CharacterResolver charResolver);
 
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Tue May 28 06:10:45 2019] You hit yourself for 8000 points of unresistable damage by Cannibalization V.")));
+            TrackLine(tracker, "[Tue May 28 06:10:45 2019] You hit yourself for 8000 points of unresistable damage by Cannibalization V.");
 
             Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khadaji"));
         }
@@ -53,7 +59,7 @@ namespace BizObjectsTests
         {
             var tracker = SetupNewTracker(out CharacterResolver charResolver);
 
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Fri May 24 18:26:18 2019] [86 Spiritwatcher (Shaman)] Khronick (Barbarian) <Siblings of the Shroud> ZONE: arthicrex")));
+            TrackLine(tracker, "[Fri May 24 18:26:18 2019] [86 Spiritwatcher (Shaman)] Khronick (Barbarian) <Siblings of the Shroud> ZONE: arthicrex");
 
             Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khronick"));
         }
@@ -63,10 +69,10 @@ namespace BizObjectsTests
         {
             var tracker = SetupNewTracker(out CharacterResolver charResolver);
 
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Fri May 24 18:26:18 2019] [86 Spiritwatcher (Shaman)] Khronick (Barbarian) <Siblings of the Shroud> ZONE: arthicrex")));
+            TrackLine(tracker, "[Fri May 24 18:26:18 2019] [86 Spiritwatcher (Shaman)] Khronick (Barbarian) <Siblings of the Shroud> ZONE: arthicrex");
 
             // Fake line, but it would set Khronick to an NPC all by itself
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Mon May 27 06:40:15 2019] You bite Khronick for 2877 points of damage.")));
+            TrackLine(tracker, "[Mon May 27 06:40:15 2019] You bite Khronick for 2877 points of damage.");
 
             Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khronick"));
         }
@@ -80,7 +86,7 @@ namespace BizObjectsTests
             charResolver.SetPlayer("Khronick");
 
             // A heal involving another player defaults to a Mercenary (It could be a player, but those should be picked up via the Who command previously)
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Mon May 27 06:40:40 2019] Khronick glows with health. Movanna healed Khronick for 7034 (7296) hit points by Healing Splash.")));
+            TrackLine(tracker, "[Mon May 27 06:40:40 2019] Khronick glows with health. Movanna healed Khronick for 7034 (7296) hit points by Healing Splash.");
 
             Assert.AreEqual(CharacterResolver.Type.Mercenary, charResolver.WhichType("Movanna"));
         }
@@ -95,7 +101,7 @@ namespace BizObjectsTests
             charResolver.SetPlayer("Movanna"); // We'll pretend this is a player already detected ... we're also not using the Sticky flag, which we already know would force it not to change
 
             // This heal, because it's a player, should stay as a player
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Mon May 27 06:40:40 2019] Khronick glows with health. Movanna healed Khronick for 7034 (7296) hit points by Healing Splash.")));
+            TrackLine(tracker, "[Mon May 27 06:40:40 2019] Khronick glows with health. Movanna healed Khronick for 7034 (7296) hit points by Healing Splash.");
 
             Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Movanna"));
         }
