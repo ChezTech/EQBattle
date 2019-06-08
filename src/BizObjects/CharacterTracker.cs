f0f692b5befc75108928e10059cc4c1123ea9a82
Joshua Gage <josh@chezgage.com>
2019-06-07 17:44:06 -0700

    Add tests to identify Players from Who line and from Heals

--- src/BizObjects/CharacterTracker.cs
+++ src/BizObjects/CharacterTracker.cs
@@ -12,6 +12,7 @@ namespace BizObjects
         private const string PetPhrase = "My leader is";
         private readonly YouResolver YouAre;
         private readonly CharacterResolver CharResolver;
+        private static List<CharacterResolver.Type> GoodCharacterTypes = new List<CharacterResolver.Type>() { CharacterResolver.Type.Player, CharacterResolver.Type.Mercenary, CharacterResolver.Type.Pet };
 
         public CharacterTracker(YouResolver youAre, CharacterResolver charResolver)
         {
@@ -34,6 +35,26 @@ namespace BizObjects
                 CharResolver.SetNonPlayer(line.Defender);
             if (YouAre.IsThisYou(line.Defender) && !YouAre.IsThisYou(line.Attacker))
                 CharResolver.SetNonPlayer(line.Attacker);
+        }
+
+        private bool IsGoodPlayer(Character c) => GoodCharacterTypes.Contains(CharResolver.WhichType(c));
+
+        public void TrackLine(Heal line)
+        {
+            TrackCharacter(line.Healer);
+            TrackCharacter(line.Patient);
+
+            // If "You" are healing/being healed then the other character is a PC or Merc
+            if (YouAre.IsThisYou(line.Healer))
+                CharResolver.SetMercenary(line.Patient);
+            if (YouAre.IsThisYou(line.Patient))
+                CharResolver.SetMercenary(line.Healer);
+
+            // If either char is a PC/merc/pet, then the other is a Merc. This won't override a Player type (if detect by /who line)
+            if (IsGoodPlayer(line.Healer))
+                CharResolver.SetMercenary(line.Patient, overwrite: false);
+            if (IsGoodPlayer(line.Patient))
+                CharResolver.SetMercenary(line.Healer, overwrite: false);
         }
 
         public void TrackLine(Song line)
@@ -76,7 +97,8 @@ namespace BizObjects
 
         public void TrackLine(Who whoLine)
         {
-            CharResolver.SetPlayer(whoLine.Character);
+            // Anyone listed via the "/who" command will be Player Characters
+            CharResolver.SetPlayer(whoLine.Character, true);
         }
 
         private void TrackCharacter(Character c)
--- src/UnitTests/BizObjectsTests/CharacterTrackerTests.cs
+++ src/UnitTests/BizObjectsTests/CharacterTrackerTests.cs
@@ -16,6 +16,7 @@ namespace BizObjectsTests
         private readonly IParser _missParser = new MissParser(YouAre);
         private readonly IParser _healParser = new HealParser(YouAre);
         private readonly IParser _killParser = new KillParser(YouAre);
+        private readonly IParser _whoParser = new WhoParser(YouAre);
 
         public CharacterTrackerTests()
         {
@@ -23,6 +24,7 @@ namespace BizObjectsTests
             _parser.AddParser(_missParser, null);
             _parser.AddParser(_healParser, null);
             _parser.AddParser(_killParser, null);
+            _parser.AddParser(_whoParser, null);
         }
 
         [TestMethod]
@@ -51,6 +53,18 @@ namespace BizObjectsTests
         }
 
         [TestMethod]
+        public void WhoLineSetsPlayer()
+        {
+            CharacterResolver charResolver = new CharacterResolver();
+            charResolver.SetPlayer(YouAre.Name); // This is normally done at the start of a Battle
+
+            var tracker = new CharacterTracker(YouAre, charResolver);
+            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Fri May 24 18:26:18 2019] [86 Spiritwatcher (Shaman)] Khronick (Barbarian) <Siblings of the Shroud> ZONE: arthicrex")));
+
+            Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khronick"));
+        }
+
+        [TestMethod]
         public void WhoLineSticks()
         {
             CharacterResolver charResolver = new CharacterResolver();
@@ -59,19 +73,52 @@ namespace BizObjectsTests
             var tracker = new CharacterTracker(YouAre, charResolver);
             tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Fri May 24 18:26:18 2019] [86 Spiritwatcher (Shaman)] Khronick (Barbarian) <Siblings of the Shroud> ZONE: arthicrex")));
 
-            Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khadaji"));
+            // Fake line, but it would set Khronick to an NPC all by itself
+            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Mon May 27 06:40:15 2019] You bite Khronick for 2877 points of damage.")));
+
+            Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khronick"));
         }
 
         [TestMethod]
-        public void WhoLineDetectsMercenary()
+        public void HealOfPlayerDetectsMercenary()
         {
             CharacterResolver charResolver = new CharacterResolver();
             charResolver.SetPlayer(YouAre.Name); // This is normally done at the start of a Battle
 
             var tracker = new CharacterTracker(YouAre, charResolver);
-            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Fri May 24 18:26:18 2019] [86 Spiritwatcher (Shaman)] Khronick (Barbarian) <Siblings of the Shroud> ZONE: arthicrex")));
 
-            Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Khadaji"));
+            // Set the player
+            charResolver.SetPlayer("Khronick");
+
+            // A heal involving another player defaults to a Mercenary (It could be a player, but those should be picked up via the Who command previously)
+            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Mon May 27 06:40:40 2019] Khronick glows with health. Movanna healed Khronick for 7034 (7296) hit points by Healing Splash.")));
+
+            Assert.AreEqual(CharacterResolver.Type.Mercenary, charResolver.WhichType("Movanna"));
+        }
+
+        [TestMethod]
+        public void HealOfPlayerDoesntOverrideExistingPlayer()
+        {
+            CharacterResolver charResolver = new CharacterResolver();
+            charResolver.SetPlayer(YouAre.Name); // This is normally done at the start of a Battle
+
+            var tracker = new CharacterTracker(YouAre, charResolver);
+
+            // Set the player
+            charResolver.SetPlayer("Khronick");
+            charResolver.SetPlayer("Movanna"); // We'll pretend this is a player already detected ... we're also not using the Sticky flag, which we already know would force it not to change
+
+            // This heal, because it's a player, should stay as a player
+            tracker.TrackLine((dynamic)_parser.ParseLine(new LogDatum("[Mon May 27 06:40:40 2019] Khronick glows with health. Movanna healed Khronick for 7034 (7296) hit points by Healing Splash.")));
+
+            Assert.AreEqual(CharacterResolver.Type.Player, charResolver.WhichType("Movanna"));
+        }
+
+        [TestMethod]
+        public void DetectPetViaMasterLine()
+        {
+            // Pet says 'My leader is Bob'
+
         }
     }
 }
