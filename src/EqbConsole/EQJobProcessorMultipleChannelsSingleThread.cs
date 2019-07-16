5c6e2a68f2e8a04f66b5fd350f61e3ce1f277d8b
Joshua Gage <josh@chezgage.com>
2019-07-16 14:04:50 -0700

    This works (waits for cancel properly)

--- src/EqbConsole/EQJobProcessorMultipleChannelsSingleThread.cs
+++ src/EqbConsole/EQJobProcessorMultipleChannelsSingleThread.cs
@@ -48,18 +48,58 @@ namespace EqbConsole
             WriteMessage($"Starting to process EQBattle with {_parserCount} parsers. (EQJobProcessorMultipleChannelsSingleThread)");
 
             // Setup our worker blocks, they won't start until they receive input into their channels
-            var parseTask = RunTask(() => ParseLines(_logLinesChannel.Reader, _parsedLinesChannel.Writer));
-            var battleTask = RunTask(() => AddLinesToBattleAsync(_parsedLinesChannel.Reader, eqBattle));
+            // var parseTask = RunTask(() => ParseLines(_logLinesChannel.Reader, _parsedLinesChannel.Writer));
+            // var battleTask = RunTask(() => AddLinesToBattleAsync(_parsedLinesChannel.Reader, eqBattle));
 
             // Start our main guy up, this starts the whole pipeline flow going
-            var readTask = RunTask(() => ReadLines(logFilePath, _logLinesChannel.Writer));
+            // var readTask = RunTask(() => ReadLines(logFilePath, _logLinesChannel.Writer));
+
+
+            // // Setup our worker blocks, they'll wait until they receive input into their channels
+            // var parseTask = ParseLines(_logLinesChannel.Reader, _parsedLinesChannel.Writer);
+            // var battleTask = AddLinesToBattleAsync(_parsedLinesChannel.Reader, eqBattle);
+
+            // // Start our main guy up, this starts the whole pipeline flow going
+            var readTask = ReadLines(logFilePath, _logLinesChannel.Writer);
+
+            // var readTask = WaitingTask(logFilePath, _logLinesChannel.Writer);
 
             // Wait for everything to finish up
-            await readTask;
-            await parseTask;
-            await battleTask;
+            // await Task.WhenAll(readTask, parseTask, battleTask);
+            await Task.WhenAll(readTask);
 
             WriteMessage($"Total processing EQBattle, {_sw.Elapsed} elapsed");
+        }
+
+        private async Task WaitingTask(string logPath, ChannelWriter<LogDatum> writer)
+        {
+
+            WriteMessage("Starting WaitingTask");
+            int i = 0;
+
+
+            using (var fs = File.Open(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+            using (var sr = new StreamReader(fs))
+            {
+                while (!CancelSource.IsCancellationRequested && i < 4)
+                {
+                    i++;
+                    WriteMessage($"In WaitingTask: {i}");
+
+                    // Do some work here
+                    ReadCurrentSetOfFileLines(sr, writer);
+
+                    try
+                    {
+                        await Task.Delay(2000, CancelSource.Token); // Ensures early exit if cancelled
+                    }
+                    catch (TaskCanceledException ex)
+                    {
+                        WriteMessage($"EX: {ex.Message}");
+                    }
+                }
+            }
+            WriteMessage("Finished WaitingTask");
         }
 
         private async Task ReadLines(string logPath, ChannelWriter<LogDatum> writer)
@@ -71,32 +111,35 @@ namespace EqbConsole
             using (var fs = File.Open(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
             using (var sr = new StreamReader(fs))
             {
+                // This sets up our infinite loop of reading the file for new changes (until cancelled)
                 while (!CancelSource.IsCancellationRequested)
                 {
-                    ReadCurrentSetOfFileLines(sr, writer, out var count);
+                    var count = ReadCurrentSetOfFileLines(sr, writer);
                     totalCount += count;
 
                     try
                     {
-                        WriteMessage("Sleeping 3s");
-                        await Task.Delay(3000, CancelSource.Token); // Ensures early exit if cancelled
+                        // WriteMessage("Sleeping 3s");
+                        await Task.Delay(500, CancelSource.Token); // Ensures early exit if cancelled
                     }
                     catch (TaskCanceledException ex)
                     {
-                        WriteMessage($"EX: {ex.Message}");
+                        WriteMessage($"ReadLine EX: {ex.Message}");
                     }
                 }
             }
             WriteMessage($"Total Lines read: {totalCount:N0}");
+            WriteMessage($"IsCancelled? {CancelSource.IsCancellationRequested}");
 
-            writer.Complete(); // We won't do this if this is a live file that's still being written to (how do we tell? Do we need to know?)
+            // We've been cancelled at this point, close out the channel
+            writer.Complete();
         }
 
-        private void ReadCurrentSetOfFileLines(StreamReader sr, ChannelWriter<LogDatum> writer, out int count)
+        private int ReadCurrentSetOfFileLines(StreamReader sr, ChannelWriter<LogDatum> writer)
         {
             string line;
             int sessionCount = 0;
-            count = 0;
+            var count = 0;
 
             while ((line = sr.ReadLine()) != null && !CancelSource.IsCancellationRequested)
             {
@@ -117,6 +160,8 @@ namespace EqbConsole
 
             if (sessionCount > 0)
                 WriteMessage($"Lines read: {count:N0} / {sessionCount:N0}, {_sw.Elapsed} elapsed");
+
+            return count;
         }
 
         private async Task ParseLines(ChannelReader<LogDatum> reader, ChannelWriter<ILine> writer)
@@ -125,9 +170,14 @@ namespace EqbConsole
             // https://gist.github.com/AlgorithmsAreCool/b0960ce8a3400305e43fe8ffdf89b32c
             // because async methods use a state machine to handle awaits
             // it is safe to await in an infinte loop. Thank you C# compiler gods!
-            while (await reader.WaitToReadAsync() && !CancelSource.IsCancellationRequested)
+
+            // while (await reader.WaitToReadAsync() && !CancelSource.IsCancellationRequested)
+            // {
+            //     while (reader.TryRead(out var logLine) && !CancelSource.IsCancellationRequested)
+
+            while (await reader.WaitToReadAsync())
             {
-                while (reader.TryRead(out var logLine) && !CancelSource.IsCancellationRequested)
+                while (reader.TryRead(out var logLine))
                 {
                     count++;
                     var line = _parser.ParseLine(logLine);
@@ -143,10 +193,14 @@ namespace EqbConsole
         private async Task AddLinesToBattleAsync(ChannelReader<ILine> reader, Battle eqBattle)
         {
             int count = 0;
-            while (await reader.WaitToReadAsync() && !CancelSource.IsCancellationRequested)
+            // while (await reader.WaitToReadAsync() && !CancelSource.IsCancellationRequested)
+            // {
+            //     while (reader.TryRead(out var line) && !CancelSource.IsCancellationRequested)
+
+            while (await reader.WaitToReadAsync())
             {
                 ILine line;
-                while (reader.TryRead(out line) && !CancelSource.IsCancellationRequested)
+                while (reader.TryRead(out line))
                 {
                     count++;
                     eqBattle.AddLine(line);
