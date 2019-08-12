6faca4f57d83e9b0b28662e17ed3ba7eaae51e62
Joshua Gage <josh@chezgage.com>
2019-08-12 14:51:35 -0700

    Remove FileSystemWatcher. Just go into an infinite loop (with an EoFReached event) until the token is cancelled.

--- src/LogFileReader/LogReader.cs
+++ src/LogFileReader/LogReader.cs
@@ -1,134 +1,73 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace LogFileReader
 {
-    public class LogReader : IDisposable
+    public class LogReader
     {
-        // Thanks: https://stackoverflow.com/a/24993767
+        private readonly CancellationToken _token;
 
-        FileSystemWatcher _fsw = new FileSystemWatcher();
-        AutoResetEvent _readEvent = new AutoResetEvent(false);
-        bool _cancelRequested = false;
+        private readonly FileInfo _logFile;
 
-        public FileInfo LogFile { get; set; }
-
-        public LogReader(string logFilePathName)
+        public LogReader(string logFilePathName, CancellationToken token)
         {
-            LogFile = new FileInfo(logFilePathName);
+            _logFile = new FileInfo(logFilePathName);
+            if (!_logFile.Exists)
+                throw new FileNotFoundException($"File not found: {logFilePathName}", logFilePathName);
 
-            if (!LogFile.Exists)
-                throw new FileNotFoundException("File does not exist", logFilePathName);
-
-            _fsw.Path = LogFile.DirectoryName;
-            _fsw.Filter = LogFile.Name;
-            _fsw.Changed += _fsw_Changed;
+            _token = token;
         }
 
-        private void _fsw_Changed(object sender, FileSystemEventArgs e)
+        public event Action<string> LineRead;
+        public event Func<Task> EoFReached;
+
+        public async Task StartReadingAsync()
         {
-            // We're only watching one file, event args will always be the same, this is pretty much telling us the file changed and we need to keep reading
-            _readEvent.Set();
+            // Force an early async context switch, otherwise our normal context switch path is only on EOF.
+            await Task.Yield();
+
+            await OpenFileToReadAsync();
         }
 
-        public event EventHandler<LineReadArgs> LineRead;
-        public event EventHandler EoFReached;
-
-        public void StartReading()
+        private async Task OpenFileToReadAsync()
         {
-            _cancelRequested = false;
-            StartWatching();
-            OpenFileToRead();
-        }
-
-        public void StopReading()
-        {
-            _cancelRequested = true;
-            StopWatching();
-        }
-
-        private void OpenFileToRead()
-        {
-            // First, read the whole file till the end (maybe later, we can just jump backwards till we find a good starting point, e.g. 2h ago)
-            // Once we hit the end, start the FSW to monitor
-
-            using (var fs = LogFile.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+            _token.ThrowIfCancellationRequested();
+            using (var fs = _logFile.Open(FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
             using (var sr = new StreamReader(fs))
-            {
-                string logLine;
-                while (!_cancelRequested)
-                {
-                    logLine = sr.ReadLine();
+                await ReadUntillCancelledAsync(sr);
+        }
 
-                    if (logLine != null)
-                        RaiseReadLine(logLine);
-                    else
-                    {
-                        RaiseEof();
-                        if (!_cancelRequested)
-                            _readEvent.WaitOne(1000);
-                    }
-                }
+        private async Task ReadUntillCancelledAsync(StreamReader sr)
+        {
+            while (!_token.IsCancellationRequested)
+            {
+                ReadCurrentLinesUntilEof(sr);
+                _token.ThrowIfCancellationRequested();
+                await RaiseEof();
             }
         }
 
-        private void StartWatching()
+        private void ReadCurrentLinesUntilEof(StreamReader sr)
         {
-            _fsw.EnableRaisingEvents = true;
-        }
-
-        private void StopWatching()
-        {
-            _fsw.EnableRaisingEvents = false;
+            string logLine;
+            while (!_token.IsCancellationRequested && (logLine = sr.ReadLine()) != null)
+                RaiseReadLine(logLine);
         }
 
         private void RaiseReadLine(string logLine)
         {
-            LineRead?.Invoke(this, new LineReadArgs(logLine));
+            LineRead?.Invoke(logLine);
         }
 
-        private void RaiseEof()
+        private async Task RaiseEof()
         {
-            EoFReached?.Invoke(this, new EventArgs());
+            // Because this is an async event handler, we need to explicitly grab all the currently registered handlers,
+            // then invoke them all and await them all.
+            var taskList = EoFReached?.GetInvocationList().Select(h => ((Func<Task>)h)?.Invoke());
+            await Task.WhenAll(taskList ?? new Task[0]);
         }
-
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                    _fsw.Dispose();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~LogReader()
-        // {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
-        #endregion
-
     }
 }
