fa2dc1b478c0328963709e5821519af39c7072f6
Joshua Gage <josh@chezgage.com>
2019-08-21 18:22:24 -0700

    Convert to log messages

--- src/EqbConsole/EQJobEvenMoreChannels.cs
+++ src/EqbConsole/EQJobEvenMoreChannels.cs
@@ -9,6 +9,8 @@ using BizObjects.Lines;
 using LineParser;
 using LogFileReader;
 using LogObjects;
+using Serilog;
+using Serilog.Events;
 
 namespace EqbConsole
 {
@@ -66,44 +68,44 @@ namespace EqbConsole
 
         public override void ShowStatus()
         {
-            WriteMessage($"Job status: log lines read: {_rawLineCount,10:N0}, datum'd: {_datumLineCount,10:N0}, parsed {_parsedLineCount,10:N0}, battled: {_battleLinesCount,10:N0}, elapsed: {_sw.Elapsed}");
+            Program.WriteLog(LogEventLevel.Verbose, $"Job status: log lines read: {_rawLineCount,10:N0}, datum'd: {_datumLineCount,10:N0}, parsed {_parsedLineCount,10:N0}, battled: {_battleLinesCount,10:N0}, elapsed: {_sw.Elapsed}");
         }
 
         public async override Task StartProcessingJobAsync(string logFilePath, Battle eqBattle)
         {
             // WatchFile = false;
 
-            WriteMessage($"Starting to process EQBattle. ({this.GetType().Name})");
+            Log.Debug($"Starting to process EQBattle. ({this.GetType().Name})");
 
             var ctSource = CancelSource;
 
             var cp = new ChannelProcessor(ctSource.Token);
 
-            cp.Message += m => WriteMessage(m);
+            cp.Message += m => Log.Verbose(m);
             var datumTask = cp.Process(_rawLinesChannel.Reader, _logLinesChannel.Writer, item => TransformLogLineToDatum(item), "Datum");
-            var dtCancel = datumTask.ContinueWith(_ => WriteMessage("DatumTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
-            var dtComplete = datumTask.ContinueWith(_ => WriteMessage("DatumTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
+            var dtCancel = datumTask.ContinueWith(_ => Log.Verbose("DatumTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
+            var dtComplete = datumTask.ContinueWith(_ => Log.Verbose("DatumTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
             var dtContinue = datumTask.ContinueWith(_ =>
             {
-                WriteMessage($"DatumTask done. {_datumLineCount:N0} datum lines, {_sw.Elapsed} elapsed");
+                Log.Verbose($"DatumTask done. {_datumLineCount:N0} datum lines, {_sw.Elapsed} elapsed");
                 _logLinesChannel.Writer.TryComplete();
             });
 
             var parseTask = cp.Process(_logLinesChannel.Reader, _parsedLinesChannel.Writer, item => TransformDatumToLine(item), "Parser");
-            var ptCancel = parseTask.ContinueWith(_ => WriteMessage("ParseTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
-            var ptComplete = parseTask.ContinueWith(_ => WriteMessage("ParseTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
+            var ptCancel = parseTask.ContinueWith(_ => Log.Verbose("ParseTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
+            var ptComplete = parseTask.ContinueWith(_ => Log.Verbose("ParseTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
             var ptContinue = parseTask.ContinueWith(_ =>
             {
-                WriteMessage($"ParseTask done. {_parsedLineCount:N0} parse lines, {_sw.Elapsed} elapsed");
+                Log.Verbose($"ParseTask done. {_parsedLineCount:N0} parse lines, {_sw.Elapsed} elapsed");
                 _parsedLinesChannel.Writer.TryComplete();
             });
 
             var battleTask = cp.Process(_parsedLinesChannel.Reader, item => AddLineToBattle(item, eqBattle), "Battle");
-            var btCancel = battleTask.ContinueWith(_ => WriteMessage("BattleTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
-            var btComplete = battleTask.ContinueWith(_ => WriteMessage("BattleTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
+            var btCancel = battleTask.ContinueWith(_ => Log.Verbose("BattleTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
+            var btComplete = battleTask.ContinueWith(_ => Log.Verbose("BattleTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
             var btContinue = battleTask.ContinueWith(_ =>
             {
-                WriteMessage($"BattleTask done. {_battleLinesCount:N0} battle lines, {_sw.Elapsed} elapsed");
+                Log.Verbose($"BattleTask done. {_battleLinesCount:N0} battle lines, {_sw.Elapsed} elapsed");
             });
 
 
@@ -111,11 +113,11 @@ namespace EqbConsole
             _sw.Start();
             // var readTask = cp.Process(_rawLinesChannel.Writer, ct => ReadLogLines(logFilePath, ct));
             var readTask = ReadLogLinesWrapper(_rawLinesChannel.Writer, logFilePath, ctSource.Token, 10 * 1000);
-            var rtCancel = readTask.ContinueWith(_ => WriteMessage("ReadTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
-            var rtComplete = readTask.ContinueWith(_ => WriteMessage("ReadTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
+            var rtCancel = readTask.ContinueWith(_ => Log.Verbose("ReadTask cancelled"), TaskContinuationOptions.OnlyOnCanceled);
+            var rtComplete = readTask.ContinueWith(_ => Log.Verbose("ReadTask complete"), TaskContinuationOptions.OnlyOnRanToCompletion);
             var rtContinue = readTask.ContinueWith(_ =>
             {
-                WriteMessage($"ReadTask done. {_rawLineCount:N0} read lines, {_sw.Elapsed} elapsed");
+                Log.Verbose($"ReadTask done. {_rawLineCount:N0} read lines, {_sw.Elapsed} elapsed");
                 _rawLinesChannel.Writer.TryComplete();
             });
 
@@ -126,25 +128,21 @@ namespace EqbConsole
                 // Wait for everything to finish up
                 await Task.WhenAll(readTask, datumTask, parseTask, battleTask);
 
-                WriteMessage("All tasks that we're waiting for are done");
+                Log.Verbose("All tasks that we're waiting for are done");
             }
             catch (OperationCanceledException ex)
             {
-                WriteMessage($"EQ Job Operation Cancelled: {ex.GetType().Name} - {ex.Message}");
-                // WriteMessage($"Exception: {ex}");
-                // WriteMessage($"Inner: {ex.InnerException}");
+                Log.Verbose(ex, "EQ Job Operation Cancelled");
                 throw;
             }
             catch (Exception ex)
             {
-                WriteMessage($"EQ Job Exception: {ex.GetType().Name} - {ex.Message}");
-                // WriteMessage($"Exception: {ex}");
-                // WriteMessage($"Inner: {ex.InnerException}");
+                Log.Error(ex, "EQ Job");
                 throw;
             }
             finally
             {
-                WriteMessage("Job done");
+                Log.Verbose("Job done");
 
                 // Give all continuation tasks a chance to finish
                 await Task.WhenAll(rtContinue, dtContinue, ptContinue, btContinue);
@@ -159,21 +157,21 @@ namespace EqbConsole
                 DumpChannelInfo(_parsedLinesChannel, "_parsedLinesChannel");
 
 
-                WriteMessage($"Total processing EQBattle, {_sw.Elapsed} elapsed");
-                WriteMessage($"LastLineNumber read: {_lastLineNumber:N0}");
-                WriteMessage($"LastLineNumber added to Battle: {_lastLineAddedToBattle?.LogLine.LineNumber:N0}");
+                Log.Debug($"Total processing EQBattle, {_sw.Elapsed} elapsed");
+                Log.Verbose($"LastLineNumber read: {_lastLineNumber:N0}");
+                Log.Verbose($"LastLineNumber added to Battle: {_lastLineAddedToBattle?.LogLine.LineNumber:N0}");
                 ShowStatus();
             }
         }
 
         public static void DumpChannelInfo<T>(Channel<T> channel, string title)
         {
-            WriteMessage($"Channel: {title,-20} reader status: {channel.Reader.Completion.Status}");
+            Log.Verbose($"Channel: {title,-20} reader status: {channel.Reader.Completion.Status}");
         }
 
         private async Task ReadLogLinesWrapper(ChannelWriter<RawLogLineInfo> writer, string logPath, CancellationToken token, int delayTimeMs = 250)
         {
-            WriteMessage("Writing channel: Reader");
+            Log.Verbose("Writing channel: Reader");
             var startingLineCount = 0;
 
             var lr = new LogReader(logPath, token);
@@ -196,7 +194,7 @@ namespace EqbConsole
             {
                 var eofLineCount = _rawLineCount - startingLineCount;
                 if (eofLineCount > 0)
-                    WriteMessage($"LogReader EOF - waiting to read next chunk. {_rawLineCount:N0} read lines, {_sw.Elapsed} elapsed");
+                    Log.Verbose($"LogReader EOF - waiting to read next chunk. {_rawLineCount:N0} read lines, {_sw.Elapsed} elapsed");
 
                 startingLineCount = _rawLineCount;
 
@@ -207,29 +205,29 @@ namespace EqbConsole
             };
 
 
-            WriteMessage("About to start reading");
+            Log.Verbose("About to start reading");
             startingLineCount = _rawLineCount;
             var lrTask = lr.StartReadingAsync();
             try
             {
-                WriteMessage("Task'd LR reading");
+                Log.Verbose("Task'd LR reading");
                 await lrTask;
-                WriteMessage("LR reading completed successfully");
+                Log.Verbose("LR reading completed successfully");
 
             }
             catch (OperationCanceledException ex)
             {
-                WriteMessage($"ReadLogLinesWrapper - Operation Cancelled: {ex.GetType().Name} - {ex.Message}");
+                Log.Verbose(ex, "ReadLogLinesWrapper");
                 throw;
             }
             catch (Exception ex)
             {
-                WriteMessage($"ReadLogLinesWrapper - Job Exception: {ex.GetType().Name} - {ex.Message}");
+                Log.Error(ex, "ReadLogLinesWrapper");
                 throw;
             }
             finally
             {
-                WriteMessage("Finally done with LR reading");
+                Log.Verbose("Finally done with LR reading");
                 Program.DumpTaskInfo(lrTask, "lrTask");
             }
         }
@@ -240,7 +238,7 @@ namespace EqbConsole
             var datum = new LogDatum(lineInfo.LogLine, lineInfo.LineNumber);
 
             if (datum.LineNumber == _lastLineNumber)
-                WriteMessage($"Log lines transformed into LogDatums: {_datumLineCount:N0}, {_sw.Elapsed} elapsed");
+                Log.Verbose($"Log lines transformed into LogDatums: {_datumLineCount:N0}, {_sw.Elapsed} elapsed");
 
             return datum;
         }
@@ -251,7 +249,7 @@ namespace EqbConsole
             var line = _parser.ParseLine(datum);
 
             if (line.LogLine.LineNumber == _lastLineNumber)
-                WriteMessage($"LogDatum's parsed into ILine's: {_parsedLineCount:N0}, {_sw.Elapsed} elapsed");
+                Log.Verbose($"LogDatum's parsed into ILine's: {_parsedLineCount:N0}, {_sw.Elapsed} elapsed");
 
             return line;
         }
@@ -263,12 +261,7 @@ namespace EqbConsole
             _lastLineAddedToBattle = line;
 
             if (line.LogLine.LineNumber == _lastLineNumber)
-                WriteMessage($"Lines added to Battle: {_battleLinesCount:N0}, {_sw.Elapsed} elapsed");
-        }
-
-        private static void WriteMessage(string format, params object[] args) // DI this
-        {
-            Program.WriteMessage(format, args);
+                Log.Verbose($"Lines added to Battle: {_battleLinesCount:N0}, {_sw.Elapsed} elapsed");
         }
     }
 }
