5d9d8999b3ab6baf0dde9aa3da7c6c3f61704cea
Joshua Gage <josh@chezgage.com>
2019-10-29 00:34:53 -0700

    Add basic layout to MainWindow and define VM properties

--- src/EQBattle/ViewModels/MainWindowViewModel.cs
+++ src/EQBattle/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EQBattle.ViewModels
+﻿namespace EQBattle.ViewModels
 {
     class MainWindowViewModel : ViewModelBase
     {
+        private ViewModelBase _headerVM;
+        private ViewModelBase _listVM;
+        private ViewModelBase _detailVM;
+        private ViewModelBase _footerVM;
+
+        public MainWindowViewModel()
+        {
+            // Register view models here
+            // Ugh .. DI this
+
+            HeaderVM = new BattleHeaderViewModel();
+            FooterVM = new BattleFooterViewModel();
+            ListVM = new FightListViewModel();
+            DetailVM = new FightViewModel();
+        }
+
+        public ViewModelBase HeaderVM
+        {
+            get => _headerVM;
+            set => SetProperty(ref _headerVM, value);
+        }
+
+        public ViewModelBase ListVM
+        {
+            get => _listVM;
+            set => SetProperty(ref _listVM, value);
+        }
+
+        public ViewModelBase DetailVM
+        {
+            get => _detailVM;
+            set => SetProperty(ref _detailVM, value);
+        }
+
+        public ViewModelBase FooterVM
+        {
+            get => _footerVM;
+            set => SetProperty(ref _footerVM, value);
+        }
     }
 }
