5c9c82aa1a9814177f390beb94e61e618404edd4
Joshua Gage <josh@chezgage.com>
2019-10-29 09:25:57 -0700

    Add status bar items to Footer

--- src/EQBattle/ViewModels/BattleFooterViewModel.cs
+++ src/EQBattle/ViewModels/BattleFooterViewModel.cs
@@ -6,5 +6,12 @@ namespace EQBattle.ViewModels
 {
     class BattleFooterViewModel : ViewModelBase
     {
+        private string fileName = @"eqlog_Khadaji_test.txt";
+        private TimeSpan elapsed = new TimeSpan(0, 0, 0, 4, 807);
+        private int skirmishCount = 917;
+
+        public string FileName { get => fileName; set => SetProperty(ref fileName, value); }
+        public TimeSpan Elapsed { get => elapsed; set => SetProperty(ref elapsed, value); }
+        public int SkirmishCount { get => skirmishCount; set => SetProperty(ref skirmishCount, value); }
     }
 }
