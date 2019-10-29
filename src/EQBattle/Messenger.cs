c92e18a933b7dfcb5b921ede310a1ce01c81ba7b
Joshua Gage <josh@chezgage.com>
2019-10-29 00:30:24 -0700

    Add standard MVVM util classes

---
+++ src/EQBattle/Messenger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EQBattle
+{
+    public sealed class Messenger
+    {
+        private Dictionary<string, HashSet<Action<object>>> events = new Dictionary<string, HashSet<Action<object>>>();
+
+        public void Subscribe(string eventName, Action<object> callback)
+        {
+            if (!events.ContainsKey(eventName))
+                events.Add(eventName, new HashSet<Action<object>>());
+
+            events[eventName].Add(callback);
+        }
+
+        public void Unsubscribe(string eventName, Action<object> callback)
+        {
+            if (events.ContainsKey(eventName))
+                events[eventName].Remove(callback);
+        }
+
+        public void Publish(string eventName, object arg = default)
+        {
+            if (events.ContainsKey(eventName))
+                foreach (var callback in events[eventName])
+                    callback(arg);
+        }
+
+        #region Singleton
+
+        // Jon Skeet: https://csharpindepth.com/articles/singleton
+
+        // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit
+        static Messenger() { }
+
+        private Messenger() { }
+
+        public static Messenger Instance { get; } = new Messenger();
+
+        #endregion
+    }
+}
--- src/EQBattle/ViewModels/ViewModelBase.cs
+++ src/EQBattle/ViewModels/ViewModelBase.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace EQBattle.ViewModels
 {
-    class ViewModelBase
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
