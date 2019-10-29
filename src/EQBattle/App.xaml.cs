88219c4752c46cd6490768f6c5330dfab401563a
Joshua Gage <josh@chezgage.com>
2019-10-29 00:17:54 -0700

    Add View and ViewModel empty classes

--- src/EQBattle/App.xaml.cs
+++ src/EQBattle/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EQBattle.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,13 @@ namespace EQBattle
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var app = new MainWindow();
+            app.DataContext = new MainWindowViewModel();
+            app.Show();
+        }
     }
 }
---
+++ src/EQBattle/ViewModels/BattleHeaderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EQBattle.ViewModels
+{
+    class BattleHeaderViewModel : ViewModelBase
+    {
+    }
+}
