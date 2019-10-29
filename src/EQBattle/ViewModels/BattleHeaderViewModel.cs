2d5435ea26239dd1e573935aaf4f396ad0afa847
Joshua Gage <josh@chezgage.com>
2019-10-29 09:42:59 -0700

    Add toolbar buttons in Header

--- src/EQBattle/ViewModels/BattleHeaderViewModel.cs
+++ src/EQBattle/ViewModels/BattleHeaderViewModel.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows.Input;
 
 namespace EQBattle.ViewModels
 {
     class BattleHeaderViewModel : ViewModelBase
     {
+        private ICommand open;
+        private ICommand run;
+        private ICommand pause;
+
+        public ICommand StartBattle { get => open ?? (open = new RelayCommand(x => OpenFile())); }
+        public ICommand Run { get => run ?? (run = new RelayCommand(x => RunFileRead())); }
+        public ICommand Pause { get => pause ?? (pause = new RelayCommand(x => PauseFileRead())); }
+
+        private void OpenFile()
+        {
+        }
+
+        private void RunFileRead()
+        {
+        }
+
+        private void PauseFileRead()
+        {
+        }
     }
 }
