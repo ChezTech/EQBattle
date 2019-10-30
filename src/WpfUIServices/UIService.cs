365ee0112127829e5f721a66b705149692a24168
Joshua Gage <josh@chezgage.com>
2019-10-29 20:19:23 -0700

    Add separate project for UI Service calls (e.g. OpenFileDialog)

---
+++ src/WpfUIServices/UIService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+
+namespace WpfUIServices
+{
+    public interface IUIService
+    {
+        string GetFileNameFromOpenFileDialog(string initialDirectory = null, string filter = null, string title = null);
+    }
+
+    public class UIService : IUIService
+    {
+        public string GetFileNameFromOpenFileDialog(string initialDirectory = null, string filter = null, string title = null)
+        {
+            var ofd = new OpenFileDialog
+            {
+                InitialDirectory = initialDirectory,
+                Filter = filter,
+                Title = title,
+            };
+
+            if (ofd.ShowDialog() == false)
+                return null;
+
+            return ofd.FileName;
+        }
+
+        #region Singleton
+
+        // Jon Skeet: https://csharpindepth.com/articles/singleton
+
+        // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit
+        static UIService() { }
+
+        private UIService() { }
+
+        public static UIService Instance { get; } = new UIService();
+
+        #endregion
+    }
+}
