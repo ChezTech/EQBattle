a3fcbe58cd16ba783dcfe43844527161bce882c3
Joshua Gage <josh@chezgage.com>
2019-10-31 23:18:13 -0700

    Add base class to raise INotifyPropertyChanged

---
+++ src/BizObjects/Battle/PropertyChangeBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BizObjects.Battle
+{
+    public abstract class PropertyChangeBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
