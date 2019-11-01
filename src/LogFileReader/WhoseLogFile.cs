e0a78985a8d01666b104c9cb5363974a87e3e529
Joshua Gage <josh@chezgage.com>
2019-10-31 23:21:09 -0700

    Add class to figure out which character a log file is for.
    This helps to discover who "you" refers to in a battle

---
+++ src/LogFileReader/WhoseLogFile.cs
@@ -0,0 +1,19 @@
+﻿namespace LogFileReader
+{
+    public class WhoseLogFile
+    {
+        public string GetCharacterNameFromLogFile(string filePath)
+        {
+            // Log file has standard name format: 'eqlog_<charName>_<serverName>.txt'
+            var firstUnder = filePath.IndexOf('_');
+            if (firstUnder == -1)
+                return null;
+
+            var secondUnder = filePath.IndexOf('_', firstUnder + 1);
+            if (secondUnder == -1)
+                return null;
+
+            return filePath.Substring(firstUnder + 1, secondUnder - firstUnder - 1);
+        }
+    }
+}
