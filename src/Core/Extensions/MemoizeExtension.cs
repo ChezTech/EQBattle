2207e89401002074002d782ea8bc17a0cb93f962
Joshua Gage <josh@chezgage.com>
2019-11-03 00:21:10 -0700

    Add Memoization extension method

---
+++ src/Core/Extensions/MemoizeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class MemoizeExtension
+    {
+        public static Func<T, TResult> Memoize<T, TResult>(this Func<T, TResult> f)
+        {
+            var cache = new ConcurrentDictionary<T, TResult>();
+            return a => cache.GetOrAdd(a, f);
+        }
+    }
+}
