30374d1be4a331f326d8dd61f9850d67f43a0944
Joshua Gage <josh@chezgage.com>
2019-11-02 23:02:32 -0700

    Class to normalize names and handle generic vs named mobs

---
+++ src/BizObjects/Converters/CharacterNameNormalizer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BizObjects.Converters
+{
+    public class CharacterNameNormalizer
+    {
+        private static HashSet<string> IgnoreWords = new HashSet<string>()
+        {
+            "Of",
+            "The",
+            "A",
+            "An",
+            "of",
+            "the",
+            "a",
+            "an",
+            "pet",
+            "warder"
+        };
+
+        public string NormalizeName(string name)
+        {
+            string normalizedName = CleanName(name);
+            normalizedName = RemoveCapitalizedNameDueToSentence(normalizedName);
+            return normalizedName;
+        }
+
+        private string CleanName(string name)
+        {
+            return name
+                .Replace("A ", "a ") // Will this get only the "A monster type" at the beginning? Could use RegEx.Replace ....
+                .Replace("An ", "an ")
+
+                // Note: this is for an apostrophe corpse ... there are actual mobs that are corpses. Those names use a backtick, e.g. "a mercenary`s corpse".
+                // We don't want to remove that term from their name as it is part of their proper name, not a state of being.
+                .Replace("'s corpse", "") // dead mobs can still have a DoT in effect, "You have taken 1960 damage from Nature's Searing Wrath by a cliknar sporali farmer's corpse."
+
+                .Replace("'s", "") // apostrophe: Can we replace this by a better Regex? (E.g. "... pierced by a monster's thorns...")
+
+                ;
+        }
+
+        // Some names are capitalized due to being at the start of a sentence. For mobs named with the indefinite article ("a", "an") that's pretty easy to deal with.
+        // For generic monster names without an indefinite ariticle, it's harder to distinugish them from a named mob.
+        // E.g. "Molten steel"
+        // What we'll do is look for a lowercase to the rest of their name (excluding "of", "the") and if we find it, we'll lowercase their first word too.
+        private string RemoveCapitalizedNameDueToSentence(string name)
+        {
+            var words = name.Split(' ');
+
+            bool shouldLower = false;
+
+            // Skip the first word, since that's the one we'll lowercase if any other words are lower
+            for (int i = 1; i < words.Length; i++)
+            {
+                var word = words[i];
+
+                if (IgnoreWords.Contains(word))
+                    continue;
+
+                if (char.IsLower(word[0]))
+                    shouldLower = true;
+            }
+
+            return shouldLower
+                ? char.ToLower(name[0]) + name.Substring(1)
+                : name;
+        }
+
+        #region Singleton
+
+        // Jon Skeet: https://csharpindepth.com/articles/singleton
+
+        // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit
+        static CharacterNameNormalizer() { }
+
+        private CharacterNameNormalizer() { }
+
+        public static CharacterNameNormalizer Instance { get; } = new CharacterNameNormalizer();
+
+        #endregion
+    }
+}
---
+++ src/UnitTests/BizObjectsTests/CharacterNameNormalizerTests.cs
@@ -0,0 +1,46 @@
+﻿using BizObjects.Battle;
+using BizObjects.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BizObjectsTests
+{
+    [TestClass]
+    public class CharacterNameNormalizerTests
+    {
+        private CharacterNameNormalizer _cnn = CharacterNameNormalizer.Instance;
+
+        [DataTestMethod]
+        [DataRow("Khadaji", "Khadaji")]
+        [DataRow("Khronick's corpse", "Khronick")]
+        [DataRow("Movanna's", "Movanna")]
+        [DataRow("Khadaji`s pet", "Khadaji`s pet")]
+        [DataRow("Girnon`s warder", "Girnon`s warder")]
+        [DataRow("a Razorfiend Subduer", "a Razorfiend Subduer")]
+        [DataRow("an awakened citizen", "an awakened citizen")]
+        [DataRow("a dwarf disciple", "a dwarf disciple")]
+        [DataRow("a craet looter", "a craet looter")]
+        [DataRow("a Blade Regulant of Erillion", "a Blade Regulant of Erillion")]
+        [DataRow("a refugee of Lunanyn", "a refugee of Lunanyn")]
+        [DataRow("a mercenary`s corpse", "a mercenary`s corpse")]
+        [DataRow("A Razorfiend Subduer", "a Razorfiend Subduer")]
+        [DataRow("An awakened citizen", "an awakened citizen")]
+        [DataRow("A dwarf disciple", "a dwarf disciple")]
+        [DataRow("A craet looter", "a craet looter")]
+        [DataRow("A Blade Regulant of Erillion", "a Blade Regulant of Erillion")]
+        [DataRow("A refugee of Lunanyn", "a refugee of Lunanyn")]
+        [DataRow("A mercenary`s corpse", "a mercenary`s corpse")]
+        [DataRow("Living steel", "living steel")]
+        [DataRow("living steel", "living steel")]
+        [DataRow("The Collector", "The Collector")]
+        [DataRow("Kalkek", "Kalkek")]
+        [DataRow("Kaledor the Tide Turner", "Kaledor the Tide Turner")]
+        [DataRow("Steel Mass", "Steel Mass")]
+        [DataRow("Seed of Battle", "Seed of Battle")]
+        [DataRow("Bursin the Legend", "Bursin the Legend")]
+        [DataRow("Armor of the Dead", "Armor of the Dead")]
+        public void Tests(string name, string normalizedName)
+        {
+            Assert.AreEqual(normalizedName, _cnn.NormalizeName(name), $"Name is: {name}");
+        }
+    }
+}
