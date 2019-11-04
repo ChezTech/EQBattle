3035feb57201f6ac6f4866e2e7ff3aebc0d5ae0f
Joshua Gage <josh@chezgage.com>
2019-11-03 21:18:50 -0800

    Hook the footer VM properties up to EQJob life cycle events

--- src/EQBattle/ViewModels/BattleFooterViewModel.cs
+++ src/EQBattle/ViewModels/BattleFooterViewModel.cs
@@ -10,23 +10,19 @@ namespace EQBattle.ViewModels
         private EQJob eqJob;
         private Battle battle;
 
-        private string fileName;
-        private TimeSpan elapsed = new TimeSpan(0, 0, 0, 4, 807);
-        private int skirmishCount = 917;
-
         public BattleFooterViewModel()
         {
             Messenger.Instance.Subscribe("NewEQJob", x =>
             {
                 eqJob = x as EQJob;
-                FileName = Path.GetFileName(eqJob.FileName);
+                OnPropertyChanged(nameof(FileName));
             });
             Messenger.Instance.Subscribe("NewBattle", x => Battle = x as Battle);
         }
 
         public Battle Battle { get => battle; set => SetProperty(ref battle, value); }
-        public string FileName { get => fileName; set => SetProperty(ref fileName, value); }
-        public TimeSpan Elapsed { get => elapsed; set => SetProperty(ref elapsed, value); }
-        public int SkirmishCount { get => skirmishCount; set => SetProperty(ref skirmishCount, value); }
+        public string FileName { get => Path.GetFileName(eqJob.FileName); }
+        public TimeSpan Elapsed { get => eqJob.ProcessingElapsed; }
+        public int SkirmishCount { get => battle.Skirmishes.Count; }
     }
 }
