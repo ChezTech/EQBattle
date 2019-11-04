2277a0c2542e6e7a576a5d23358b4c5381a9f4e7
Joshua Gage <josh@chezgage.com>
2019-11-03 16:29:52 -0800

    Bare bones of Fight Detail view

--- src/EQBattle/ViewModels/FightViewModel.cs
+++ src/EQBattle/ViewModels/FightViewModel.cs
@@ -8,10 +8,12 @@ namespace EQBattle.ViewModels
     class FightViewModel : ViewModelBase
     {
         private Battle battle;
+        private Skirmish skirmish;
 
         public FightViewModel()
         {
             Messenger.Instance.Subscribe("NewBattle", x => NewBattle(x));
+            Messenger.Instance.Subscribe("OnSelectedSkirmishChanged", x => Skirmish = x as Skirmish);
         }
 
         private void NewBattle(object x)
@@ -20,5 +22,6 @@ namespace EQBattle.ViewModels
         }
 
         public Battle Battle { get => battle; set => SetProperty(ref battle, value); }
+        public Skirmish Skirmish { get => skirmish; set => SetProperty(ref skirmish, value); }
     }
 }
