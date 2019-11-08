7543610fb4231e4f245106e5059b9eb175d5080e
Joshua Gage <josh@chezgage.com>
2019-11-07 20:46:37 -0800

    Hook up tab control into Fight V/VM and select the FightOverview into it at first

---
+++ src/EQBattle/ViewModels/FightViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BizObjects.Battle;
+using Core;
+
+namespace EQBattle.ViewModels
+{
+    public class FightViewModel : PropertyChangeBase
+    {
+        private PropertyChangeBase currentFightVM;
+
+        private Skirmish skirmish;
+
+        public FightViewModel()
+        {
+            Messenger.Instance.Subscribe("OnSelectedSkirmishChanged", x => Skirmish = x as Skirmish);
+
+            CurrentFightVM = new FightOverviewViewModel();
+        }
+
+        public Skirmish Skirmish { get => skirmish; set => SetProperty(ref skirmish, value); }
+
+        public PropertyChangeBase CurrentFightVM { get => currentFightVM; set => SetProperty(ref currentFightVM, value); }
+    }
+}
