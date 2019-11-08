aa083a48a4e2e236fec509a5cb1ccea0671d44a8
Joshua Gage <josh@chezgage.com>
2019-11-07 20:45:56 -0800

    Add FightLogLines V/VM

---
+++ src/EQBattle/ViewModels/FightLogLinesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EQBattle.ViewModels
+{
+    public class FightLogLinesViewModel : PropertyChangeBase
+    {
+    }
+}
