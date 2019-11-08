28822fbbd02724b5b4244188c32dc8b9333b2d1f
Joshua Gage <josh@chezgage.com>
2019-11-07 23:02:38 -0800

    Make the Fight V/VM use binding to get the tab names and content

--- src/EQBattle/ViewModels/FightViewModel.cs
+++ src/EQBattle/ViewModels/FightViewModel.cs
@@ -1,23 +1,36 @@
 ﻿using BizObjects.Battle;
 using Core;
+using System.Collections.ObjectModel;
 
 namespace EQBattle.ViewModels
 {
     public class FightViewModel : PropertyChangeBase
     {
-        private PropertyChangeBase currentFightVM;
+        private ObservableCollection<FightTabItem> fightTabs = new ObservableCollection<FightTabItem>();
+        private FightTabItem selectedTab;
 
         private Skirmish skirmish;
+
+        public class FightTabItem
+        {
+            public string Header { get; set; }
+            public PropertyChangeBase Content { get; set; }
+        }
 
         public FightViewModel()
         {
             Messenger.Instance.Subscribe("OnSelectedSkirmishChanged", x => Skirmish = x as Skirmish);
 
-            CurrentFightVM = new FightOverviewViewModel();
+            fightTabs.Add(new FightTabItem() { Header = "Overview", Content = new FightOverviewViewModel() });
+            fightTabs.Add(new FightTabItem() { Header = "Log Lines", Content = new FightLogLinesViewModel() });
+
+            SelectedTab = fightTabs[0];
         }
 
         public Skirmish Skirmish { get => skirmish; set => SetProperty(ref skirmish, value); }
 
-        public PropertyChangeBase CurrentFightVM { get => currentFightVM; set => SetProperty(ref currentFightVM, value); }
+        public ObservableCollection<FightTabItem> FightTabs { get => fightTabs; set => SetProperty(ref fightTabs, value); }
+
+        public FightTabItem SelectedTab { get => selectedTab; set => SetProperty(ref selectedTab, value); }
     }
 }
