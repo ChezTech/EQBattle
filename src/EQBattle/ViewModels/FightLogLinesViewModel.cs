c977dc13a0b71d66b5bd63d8b007ce2cd5b57820
Joshua Gage <josh@chezgage.com>
2019-11-12 09:24:52 -0800

    Add View elements for LogLines

--- src/EQBattle/ViewModels/FightLogLinesViewModel.cs
+++ src/EQBattle/ViewModels/FightLogLinesViewModel.cs
@@ -7,5 +7,13 @@ namespace EQBattle.ViewModels
 {
     public class FightLogLinesViewModel : PropertyChangeBase
     {
+        private Fight fight;
+
+        public FightLogLinesViewModel()
+        {
+            Messenger.Instance.Subscribe("OnSelectedFightChanged", x => Fight = x as Fight);
+        }
+
+        public Fight Fight { get => fight; set => SetProperty(ref fight, value); }
     }
 }
