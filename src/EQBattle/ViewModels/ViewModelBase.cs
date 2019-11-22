b13c18be77630fbd41e1bff54ce5193c1540694c
Joshua Gage <josh@chezgage.com>
2019-11-21 20:36:46 -0800

    New VM base class for running this specifically on the UI thread

---
+++ src/EQBattle/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using Core;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace EQBattle.ViewModels
+{
+    public class ViewModelBase : PropertyChangeBase
+    {
+        protected void RunOnUIThread(Action action)
+        {
+            // If we're on the UI already, just run the thing
+            if (Application.Current.Dispatcher.Thread == Thread.CurrentThread)
+                action();
+            else
+                Application.Current.Dispatcher.BeginInvoke(action);
+        }
+    }
+}
