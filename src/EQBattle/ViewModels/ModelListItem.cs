25b62a283662bc856d8a46388719ee5ef29876b2
Joshua Gage <josh@chezgage.com>
2019-11-21 22:51:26 -0800

    Introduce a base class to help with translating a model class into a view model lass used for list items

---
+++ src/EQBattle/ViewModels/ModelListItem.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+
+namespace EQBattle.ViewModels
+{
+    public interface IModelListItem<T>
+    {
+        public T Model { get; }
+        void Refresh();
+    }
+
+    public abstract class ModelListItem<T> : PropertyChangeBase, IModelListItem<T>
+    {
+        public ModelListItem(T model)
+        {
+            Model = model;
+        }
+
+        public T Model { get; private set; }
+
+        public abstract void Refresh();
+    }
+}
