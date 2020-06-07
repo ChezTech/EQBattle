dc83287967b71d743c757a78ec6e9561925815c9
Joshua Gage <josh@chezgage.com>
2020-06-06 21:25:39 -0700

    Add new abstract view model class for views that need a list of view items from models

---
+++ src/EQBattle/ViewModels/ModelListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Core;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace EQBattle.ViewModels
+{
+    public abstract class ModelListViewModel<T, K> : PropertyChangeBase
+        where T : class
+        where K : ModelListItem<T>
+    {
+        private readonly object _lock = new object();
+
+        private ObservableCollection<K> modelListItems;
+        public ObservableCollection<K> ModelListItems { get => modelListItems; set => SetProperty(ref modelListItems, value); }
+
+        protected void NewModelListItems(IEnumerable<K> sourceModelListItems)
+        {
+            ModelListItems = new ObservableCollection<K>(sourceModelListItems);
+            BindingOperations.EnableCollectionSynchronization(ModelListItems, _lock);
+        }
+    }
+}
