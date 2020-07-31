18c7ee95becb135fd14a5a7e8c582ba288087726
Joshua Gage <josh@chezgage.com>
2020-07-30 22:15:26 -0700

    Ensure that the UI .ini files exist for this character so they get sym-linked correctly.

--- Helper Scripts/NewCharFolder/NewCharFolder.ps1
+++ Helper Scripts/NewCharFolder/NewCharFolder.ps1
@@ -3,20 +3,72 @@
 
 # ============================================================================== User Input
 $BaseEQFolder = "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest"
-$BoxCharName = "Balymoor"
+$BoxCharName = "Aiyya"
+
+# Name of the server, for writing the UI .ini files
+$ServerName = "test"
 
 # ============================================================================== Code
-$BoxEQFolder = $BaseEQFolder + "-" + $BoxCharName
 
+
+# -------------------------------------------------
+# Define variables
+
+$BoxEQFolder = $BaseEQFolder + "-" + $BoxCharName
+# C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest-Aiyya
+
+$BoxEQClient = Join-Path -Path $BoxEQFolder -ChildPath "eqclient.ini"
+# C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest-Aiyya\eqclient.ini
+
+$BaseEQClient = Join-Path -Path $BaseEQFolder -ChildPath "eqclient.ini"
+# C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\eqclient.ini
+
+$BaseCharEQClient = Join-Path -Path $BaseEQFolder -ChildPath ($BoxCharName + "_eqclient.ini")
+# C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\Aiyya_eqclient.ini
+
+$BaseCharServerIni = Join-Path -Path $BaseEQFolder -ChildPath ($BoxCharName + "_" + $ServerName + ".ini")
+# C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\Aiyya_test.ini
+
+$UIBaseCharServerIni = Join-Path -Path $BaseEQFolder -ChildPath ("UI_" + $BoxCharName + "_" + $ServerName + ".ini")
+# C:\Users\Public\Daybreak Game Company\Installed Games\EverQuest\UI_Aiyya_test.ini
+
+
+# -------------------------------------------------
+# Make sure the New Character's .ini files exist in the original folder
+
+# First, make sure the named character's 'eqclient.ini' file exists, copy the base 'eqclient.ini' if not.
+if (!(Test-Path -Path $BaseCharEQClient))
+{
+    Copy-Item -Path $BaseEQClient -Destination $BaseCharEQClient
+}
+
+# Now, make sure the other UI files exists. Create them if not. They will get sym-linked below
+
+# '<CharName>_<ServerName>.ini'
+if (!(Test-Path -Path $BaseCharServerIni))
+{
+	New-Item -ItemType File -Path $BaseCharServerIni
+}
+
+# 'UI_<CharName>_<ServerName>.ini'
+if (!(Test-Path -Path $UIBaseCharServerIni))
+{
+	New-Item -ItemType File -Path $UIBaseCharServerIni
+}
+
+
+# -------------------------------------------------
 # Create the folder
 if (!(Test-Path -Path $BoxEQFolder))
 {
     $null = New-Item -ItemType Directory -Path $BoxEQFolder
 }
 
+# -------------------------------------------------
+# Make sym links for each file and folder
+
 $HardExtCollection = ".exe", ".dll"
 
-# Make sym links for each file and folder
 $EQFiles = Get-ChildItem -Path $BaseEQFolder
 foreach ($file in $EQFiles)
 {
@@ -40,16 +92,9 @@ foreach ($file in $EQFiles)
     }
 }
 
-# Now, override the 'eqclient.ini' file to symlink to the named version in the base folder
-$BoxEQClient = Join-Path -Path $BoxEQFolder -ChildPath "eqclient.ini"
-$BaseEQClient = Join-Path -Path $BaseEQFolder -ChildPath "eqclient.ini"
-$BaseCharEQClient = Join-Path -Path $BaseEQFolder -ChildPath ($BoxCharName + "_eqclient.ini")
 
-# First, make sure the named character's 'eqclient.ini' file exists
-if (!(Test-Path -Path $BaseCharEQClient))
-{
-    Copy-Item -Path $BaseEQClient -Destination $BaseCharEQClient
-}
+# -------------------------------------------------
+# Now, override the 'eqclient.ini' file to symlink to the named version in the base folder
 
 # Remove the new folder's 'eqclient.ini' that we symlinked above to the base 'eqclient.ini'
 if (Test-Path -Path $BoxEQClient)
