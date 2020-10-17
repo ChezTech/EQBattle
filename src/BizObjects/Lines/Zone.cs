47d348c503022bb47708b008f286ec53a5ea8784
Joshua Gage <josh@chezgage.com>
2020-10-16 21:49:08 -0700

    Implement Equality for Zone class.
    This will make Battle class's ZoneLineMap actually match on Zone Name, not instance.

--- src/BizObjects/Lines/Zone.cs
+++ src/BizObjects/Lines/Zone.cs
@@ -1,8 +1,10 @@
 ﻿using LogObjects;
+using System;
+using System.Collections.Generic;
 
 namespace BizObjects.Lines
 {
-    public class Zone : Line
+    public class Zone : Line, IEquatable<Zone>
     {
         public static Zone Unknown = new Zone(null, "Unknown");
 
@@ -11,5 +13,52 @@ namespace BizObjects.Lines
             Name = zoneName;
         }
         public string Name { get; }
+
+        #region Equality
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Zone);
+        }
+
+        public bool Equals(Zone other)
+        {
+            return other != null &&
+                   Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name);
+        }
+
+        public static bool operator ==(Zone left, Zone right)
+        {
+            return EqualityComparer<Zone>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Zone left, Zone right)
+        {
+            return !(left == right);
+        }
+
+        #endregion
+
+        private string DebuggerDisplay => string.Format($"{Name}");
+
+        public override string ToString()
+        {
+            return ToString("G");
+        }
+
+        public string ToString(string format)
+        {
+            switch (format)
+            {
+                case "G":
+                default:
+                    return DebuggerDisplay;
+            }
+        }
     }
 }
